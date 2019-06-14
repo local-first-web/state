b802b9a1a6a7d99cd3c026d36ecf74dccb8d0f50
Herb Caudill <herb@devresults.com>
2019-06-14 18:20:27 -0400

    automergify: remove braces

--- src/automergify.ts
+++ src/automergify.ts
@@ -4,7 +4,5 @@ import { INITIALIZE } from './constants'
 // Builds a new automerge object from the object provided
 export const automergify = <T>(obj: T) =>
   automerge.change(automerge.init<T>(), INITIALIZE, (d: T) => {
-    for (const k in obj) {
-      d[k] = obj[k]
-    }
+    for (const k in obj) d[k] = obj[k]
   })
