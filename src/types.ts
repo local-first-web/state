3427484d842604b7bcedc9b07ef501d604c69160
Herb Caudill <herb@devresults.com>
2019-06-14 21:37:57 -0400

    add todos

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,11 @@
 import { Reducer, Middleware, AnyAction } from 'redux'
 
 export type ProxyReducer<T> = (action: AnyAction) => ChangeFn<T> | null
-export type ReducerAdapter = <T>(proxy: ProxyReducer<T>) => Reducer<T | undefined, AnyAction>
 
-// HACK - after building, can't get it to pick up the Feed type from the ambient hypercore types
+export type ReducerConverter = <T>(proxy: ProxyReducer<T>) => Reducer
+
+// TODO: sort out the type for feed
+// after building, can't get it to pick up the Feed type from the ambient hypercore types
 export type MiddlewareFactory = (feed: any) => Middleware
 // export type MiddlewareFactory = (feed: Feed<string>) => Middleware
 
@@ -15,7 +17,8 @@ export interface CreateStoreOptions {
   // Redux store
   proxyReducer: ProxyReducer<any>
   preloadedState?: any
-  middlewares?: Middleware[]
+  middlewares?: Middleware[] // TODO: accept an `enhancer` object instead
+
   // hypercore feed options
   key: string
   secretKey: string
