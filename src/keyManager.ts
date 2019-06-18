c00d102ff7e2f73d053a628bca7fbcbf201560cd
Brent Keller <brentkeller@gmail.com>
2019-06-18 17:25:14 -0400

    Create keyManager file for key-related methods

--- src/keyManager.ts
+++ src/keyManager.ts
@@ -1,3 +1,7 @@
+import crypto from 'hypercore-crypto'
+
+export const generateKeys = () => crypto.keyPair()
+
 export const validateKeys = (key?: string, secretKey?: string) => {
   if (!key || !secretKey || key.length !== 64 || secretKey.length !== 128) return false
   // TODO: Figure out why Jest is choking on BigInt
