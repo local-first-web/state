a1c18b201962738899e73db2dd8e08e1cd0af957
Brent Keller <brentkeller@gmail.com>
2019-06-20 16:53:47 -0400

    Append local actions to the feed
    This gets local persistence working again

--- core/src/getMiddleware.ts
+++ core/src/getMiddleware.ts
@@ -2,12 +2,9 @@ import { MiddlewareFactory } from './types'
 import automerge, { DocSet } from 'automerge'
 import debug from 'debug'
 import { DOC_ID } from './constants'
-import { CevitxeConnection } from './connection'
 const log = debug('cevitxe:todo')
 
 export const getMiddleware: MiddlewareFactory = feed => store => next => action => {
-  // console.group('cevitxe: ' + action.type)
-
   // before changes
   const prevState = store.getState() || new DocSet()
   const prevDoc = prevState.getDoc(DOC_ID) || automerge.init()
@@ -21,17 +18,17 @@ export const getMiddleware: MiddlewareFactory = feed => store => next => action 
   const nextState = store.getState()
   const nextDoc = nextState.getDoc(DOC_ID)
 
-  // Maybe we don't need middleware any more since DocSet already responds to changes?
+  // write actions to feed (if they're not already coming from the feed)
+  if (!action.payload.cameFromFeed) {
+    const changes = automerge.getChanges(prevDoc, nextDoc)
+    const message = {
+      docId: DOC_ID,
+      clock: nextDoc.map,
+      changes,
+    }
 
-  // // write actions to feed (if they're not already coming from the feed)
-  // if (!action.payload.cameFromFeed) {
-  //   // const changes = automerge.getChanges(prevDoc, nextDoc)
-  //   // changes.forEach(change => feed.append(JSON.stringify(change)))
+    feed.append(JSON.stringify(message)
+  }
 
-  //   const connections: CevitxeConnection[] = []
-  //   connections.forEach(connection => {
-  //     connection.docSet.setDoc(DOC_ID, nextDoc)
-  //   })
-  // }
   return result
 }
