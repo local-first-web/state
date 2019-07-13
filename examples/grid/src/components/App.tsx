4cd766ccbb7ae0efd39876ca5f34e1b8a248d915
Herb Caudill <herb@devresults.com>
2019-07-13 12:55:08 +0200

    housekeeping

--- examples/grid/src/components/App.tsx
+++ examples/grid/src/components/App.tsx
@@ -4,14 +4,7 @@ import List from './List'
 import { useSelector } from 'react-redux'
 import { Loading } from './Loading'
 
-const App: React.FC = () => {
+export const App = () => {
   const ready = useSelector(state => !!state)
-  return (
-    <div className="App">
-      {/**/}
-      {ready ? <List /> : <Loading />}
-    </div>
-  )
+  return <div className="App">{ready ? <List /> : <Loading />}</div>
 }
-
-export default App
