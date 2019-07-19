ceb1c6df3c577c770b77026d50f3588ac8b175f7
Herb Caudill <herb@devresults.com>
2019-07-19 14:50:10 +0200

    friendly-words types

--- packages/toolbar/src/wordPair.ts
+++ packages/toolbar/src/wordPair.ts
@@ -1,7 +1,7 @@
-const friendlyWords = require('friendly-words')
+import friendlyWords from 'friendly-words'
 
 const { predicates, objects } = friendlyWords
 
 const randomElement = (arr: string[]) => arr[Math.floor(Math.random() * arr.length)]
 
-export const wordPair = () => randomElement(predicates) + '-' + randomElement(objects)
+export const wordPair = (): string => randomElement(predicates) + '-' + randomElement(objects)
