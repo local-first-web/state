37be851e58f0a91606e3677e1ba8d8c17f9f860c
Herb Caudill <herb@devresults.com>
2019-07-20 13:29:51 +0200

    core/Cevitxe.test: couple more passing tests

--- packages/core/src/Cevitxe.test.ts
+++ packages/core/src/Cevitxe.test.ts
@@ -29,18 +29,14 @@ describe('Cevitxe', () => {
   let documentId: string
   const initialState: FooState = { foo: 1 }
   let store: Store
-  const port = 10003
+  const port = 10004
   const urls = [`http://localhost/${port}`]
 
   let server
 
-  it('should ', () => {
-    expect(true).toBeTruthy()
-  })
-
   beforeAll(async () => {
     server = new Server({ port })
-    await server.listen()
+    await server.listen({ silent: true })
   })
 
   beforeEach(() => {
@@ -60,92 +56,93 @@ describe('Cevitxe', () => {
     expect(store.getState()).toEqual({})
   })
 
-  // describe('connections enabled', () => {
-  //   beforeEach(async () => {
-  //     store = await cevitxe.createStore(documentId)
-  //   })
+  describe('connections enabled', () => {
+    beforeEach(async () => {
+      store = await cevitxe.createStore(documentId)
+    })
 
-  //   afterEach(() => {
-  //     log('afterEach')
-  //   })
+    afterEach(() => {
+      log('afterEach')
+    })
 
-  //   it('createStore should return a connected redux store', async () => {
-  //     expect.assertions(2)
-  //     expect(store).not.toBeUndefined()
-  //     expect(store.getState()).toEqual(A.from(initialState))
-  //   })
+    it('createStore should return a connected redux store', async () => {
+      expect.assertions(2)
+      expect(store).not.toBeUndefined()
+      expect(store.getState()).toEqual(A.from(initialState))
+    })
 
-  //   it('should use the reducer we gave it', () => {
-  //     store.dispatch({ type: 'SET_FOO', payload: { value: 3 } })
-  //     const doc = store.getState()
-  //     expect(doc.foo).toEqual(3)
-  //   })
+    it('should use the reducer we gave it', () => {
+      store.dispatch({ type: 'SET_FOO', payload: { value: 3 } })
+      const doc = store.getState()
+      expect(doc.foo).toEqual(3)
+    })
 
-  //   it('should return something that looks like a store', () => {
-  //     expect(store).toHaveProperty('getState')
-  //     expect(store).toHaveProperty('dispatch')
-  //     expect(store).toHaveProperty('subscribe')
-  //   })
+    it('should return something that looks like a store', () => {
+      expect(store).toHaveProperty('getState')
+      expect(store).toHaveProperty('dispatch')
+      expect(store).toHaveProperty('subscribe')
+    })
 
-  //   it('{FLAKY TEST} should communicate changes from one store to another', async done => {
-  //     // instantiate remote store
+    // it('{FLAKY TEST} should communicate changes from one store to another', async done => {
+    //   // instantiate remote store
 
-  //     const remoteCevitxe = new Cevitxe({
-  //       proxyReducer,
-  //       initialState: {},
-  //       onReceive,
-  //       databaseName: 'remote-store',
-  //     })
+    //   const remoteCevitxe = new Cevitxe({
+    //     proxyReducer,
+    //     initialState: {},
+    //     onReceive,
+    //     databaseName: 'remote-store',
+    //   })
 
-  //     remoteCevitxe.joinStore(documentId)
-  //     // We're going to intentionally delay changes to the local store,
-  //     // this allows us to test receiving of the initial state and additional changes
-  //     let receiveCount = 0
-  //     function onReceive(message: any) {
-  //       if (receiveCount === 0) expect(store.getState().foo).toEqual(1)
-  //       if (receiveCount === 1) {
-  //         expect(store.getState().foo).toEqual(42)
-  //         done()
-  //       }
-  //       receiveCount++
-  //     }
-  //     // Delay new change to the local store so remote gets 2 separate messages
-  //     await pause(1000)
-  //     // change something in the local store
-  //     store.dispatch({ type: 'SET_FOO', payload: { value: 42 } })
-  //   })
-  // })
-
-  // describe('connections disabled', () => {
-  //   beforeEach(async () => {
-  //     webrtcSwarm._setEnablePeerConnections(false)
-  //     store = await cevitxe.createStore(documentId)
-  //   })
-
-  //   // To simulate rehydrating from persisted state we create an initial store and
-  //   // add some changes. This state gets written to our fake-indexeddb.
-  //   // Then we close the current feed and create a new store, which should see the
-  //   // state in the fake db and load it
-  //   it('should rehydrate from persisted state when available', async done => {
-  //     // make changes to local state
-  //     store.dispatch({
-  //       type: 'SET_FOO',
-  //       payload: {
-  //         value: 42,
-  //       },
-  //     })
-  //     expect(store.getState().foo).toBe(42)
-  //     // Short wait to let storage finish writing
-  //     await pause(100)
-  //     // disconnect current store
-  //     // await cevitxe.close()
-
-  //     // // create a new store and verify it has previous state from storage
-  //     // const newStore = await cevitxe.createStore(documentId)
-  //     // expect(store.getState().foo).toBe(42)
-  //     done()
-  //   })
+    //   remoteCevitxe.joinStore(documentId)
+    //   // We're going to intentionally delay changes to the local store,
+    //   // this allows us to test receiving of the initial state and additional changes
+    //   let receiveCount = 0
+    //   function onReceive(message: any) {
+    //     if (receiveCount === 0) expect(store.getState().foo).toEqual(1)
+    //     if (receiveCount === 1) {
+    //       expect(store.getState().foo).toEqual(42)
+    //       done()
+    //     }
+    //     receiveCount++
+    //   }
+    //   // Delay new change to the local store so remote gets 2 separate messages
+    //   await pause(1000)
+    //   // change something in the local store
+    //   store.dispatch({ type: 'SET_FOO', payload: { value: 42 } })
+    // })
+  })
 })
+
+//   describe('connections disabled', () => {
+//     beforeEach(async () => {
+//       webrtcSwarm._setEnablePeerConnections(false)
+//       store = await cevitxe.createStore(documentId)
+//     })
+
+//     // To simulate rehydrating from persisted state we create an initial store and
+//     // add some changes. This state gets written to our fake-indexeddb.
+//     // Then we close the current feed and create a new store, which should see the
+//     // state in the fake db and load it
+//     it('should rehydrate from persisted state when available', async done => {
+//       // make changes to local state
+//       store.dispatch({
+//         type: 'SET_FOO',
+//         payload: {
+//           value: 42,
+//         },
+//       })
+//       expect(store.getState().foo).toBe(42)
+//       // Short wait to let storage finish writing
+//       await pause(100)
+//       // disconnect current store
+//       // await cevitxe.close()
+
+//       // // create a new store and verify it has previous state from storage
+//       // const newStore = await cevitxe.createStore(documentId)
+//       // expect(store.getState().foo).toBe(42)
+//       done()
+//     })
+// })
 
 //   it.skip('close should destroy any current store', async () => {
 //     expect.assertions(2)
