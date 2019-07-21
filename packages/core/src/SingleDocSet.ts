f7dbd8e9038bba45d774e3711cb5e543b01244ce
Herb Caudill <herb@devresults.com>
2019-07-21 18:29:32 +0200

    core/SingleDocSet.ts: add logging; use something small & random as our id to facilitate debugging

--- packages/core/src/SingleDocSet.ts
+++ packages/core/src/SingleDocSet.ts
@@ -1,31 +1,37 @@
-import A from 'automerge'
+import A, { uuid } from 'automerge'
+import debug from 'debug'
 
 // The `automerge.Connection` class takes an `automerge.DocSet`, which is designed to manage
 // multiple documents. At this point we're only using a single document, so rather than have to keep
 // track of a consistent document ID everywhere we just use this thin wrapper that just exposes a
 // single document via `singleDocSet.set` and `singleDocSet.get`. The underlying `DocSet` is
 // available as `singleDocSet.base`.
-
 export class SingleDocSet<T> {
-  // This could be anything
-  private DOC_ID = '1'
+  public id: string
+
+  private log: (...args: any[]) => void
 
   // The underlying DocSet is available as `base`, for properties that we're not otherwise exposing
   public base: A.DocSet<T>
 
   // example: `const docSet = new SingleDocSet(state)`
   constructor(doc: A.Doc<T>) {
+    this.id = uuid().slice(0, 4)
+    this.log = debug(`cevitxe:singledocset:${this.id}`)
+    this.log('new')
     this.base = new A.DocSet()
     this.set(doc)
   }
 
   // example: `docSet.set(state)`
   public set(doc: A.Doc<T>) {
-    this.base.setDoc(this.DOC_ID, doc)
+    this.log('set')
+    this.base.setDoc(this.id, doc)
   }
 
   // example: `const newState = docSet.get()`
   public get() {
-    return this.base.getDoc(this.DOC_ID)
+    this.log('get')
+    return this.base.getDoc(this.id)
   }
 }
