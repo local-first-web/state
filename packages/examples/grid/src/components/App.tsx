c7056c69485c88f2ad48ccfe7103c156536e6b20
Diego Mijelshon <diego@mijelshon.com.ar>
2019-07-22 12:53:46 -0300

    Import functions for demo

--- packages/examples/grid/src/components/App.tsx
+++ packages/examples/grid/src/components/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { ChangeEvent } from 'react'
 import './App.css'
 import List from './List'
-import { useSelector } from 'react-redux'
+import { loadSchema, loadCollection } from '../redux/actions'
+import { useSelector, useDispatch } from 'react-redux'
 import { Loading } from './Loading'
 
 export const App = () => {
   const ready = useSelector(state => !!state)
-  return <>{ready ? <List /> : <Loading />}</>
+  const dispatch = useDispatch()
+
+  const loader = (cb: any) => (event: ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader()
+    reader.onload = progress => dispatch(cb(JSON.parse((progress.target as any).result)))
+    reader.readAsText(event.target.files![0])
+  }
+  return ready ? (
+    <>
+      <div>
+        Schema: <input type="file" onChange={loader(loadSchema)} />
+        Data: <input type="file" onChange={loader(loadCollection)} />
+      </div>
+      <List />
+    </>
+  ) : (
+    <Loading />
+  )
 }
--- packages/examples/grid/src/redux/actions.ts
+++ packages/examples/grid/src/redux/actions.ts
@@ -1,47 +1,47 @@
-import uuid from "uuid";
+import uuid from 'uuid'
 
-export const SCHEMA_LOAD = "SCHEMA_LOAD";
-export const FIELD_ADD = "FIELD_ADD";
-export const FIELD_RENAME = "FIELD_RENAME";
-export const FIELD_DELETE = "FIELD_DELETE";
-export const FIELD_SET_TYPE = "FIELD_SET_TYPE";
-export const COLLECTION_LOAD = "ITEMS_LOAD";
-export const ITEM_ADD = "ITEM_ADD";
-export const ITEM_UPDATE = "ITEM_UPDATE";
-export const ITEM_REMOVE = "ITEM_REMOVE";
+export const SCHEMA_LOAD = 'SCHEMA_LOAD'
+export const FIELD_ADD = 'FIELD_ADD'
+export const FIELD_RENAME = 'FIELD_RENAME'
+export const FIELD_DELETE = 'FIELD_DELETE'
+export const FIELD_SET_TYPE = 'FIELD_SET_TYPE'
+export const COLLECTION_LOAD = 'ITEMS_LOAD'
+export const ITEM_ADD = 'ITEM_ADD'
+export const ITEM_UPDATE = 'ITEM_UPDATE'
+export const ITEM_REMOVE = 'ITEM_REMOVE'
 
 export function loadSchema(schema: any[]) {
-  return { type: SCHEMA_LOAD, schema };
+  return { type: SCHEMA_LOAD, payload: { schema } }
 }
 
 export function addField() {
-  return { type: FIELD_ADD, payload: { id: uuid() } };
+  return { type: FIELD_ADD, payload: { id: uuid() } }
 }
 
 export function renameField(id: string, description: string) {
-  return { type: FIELD_RENAME, payload: { id, description } };
+  return { type: FIELD_RENAME, payload: { id, description } }
 }
 
 export function deleteField(id: string) {
-  return { type: FIELD_DELETE, payload: { id } };
+  return { type: FIELD_DELETE, payload: { id } }
 }
 
 export function setFieldType(id: string, type: string) {
-  return { type: FIELD_SET_TYPE, payload: { id, type } };
+  return { type: FIELD_SET_TYPE, payload: { id, type } }
 }
 
 export function loadCollection(collection: any[]) {
-  return { type: COLLECTION_LOAD, collection };
+  return { type: COLLECTION_LOAD, payload: { collection } }
 }
 
 export function addItem() {
-  return { type: ITEM_ADD, payload: { id: uuid() } };
+  return { type: ITEM_ADD, payload: { id: uuid() } }
 }
 
 export function updateItem(id: string, field: string, value: any) {
-  return { type: ITEM_UPDATE, payload: { id, field, value } };
+  return { type: ITEM_UPDATE, payload: { id, field, value } }
 }
 
 export function removeItem(id: string) {
-  return { type: ITEM_REMOVE, payload: { id } };
+  return { type: ITEM_REMOVE, payload: { id } }
 }
