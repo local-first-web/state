52e5d6396e2f21bd7665c99afd9ad4a2e826a992
Diego Mijelshon <diego@mijelshon.com.ar>
2019-07-24 11:05:20 -0300

    Exclude id from schema

--- packages/examples/grid/src/inferSchema.ts
+++ packages/examples/grid/src/inferSchema.ts
@@ -4,12 +4,13 @@ import uuid from 'uuid'
 export function inferSchema(sampleData: any): JSONSchema7 {
   //no error handling or anything for now
   const firstRow = Object.values<any>(sampleData)[0]
+  const visiblePropertyEntries = Object.entries<any>(firstRow).filter(([name]) => name !== 'id')
   return {
     $schema: 'http://json-schema.org/draft-07/schema#',
     $id: `https://factbook.app/${uuid()}.json`,
     type: 'object',
     properties: Object.fromEntries(
-      Object.entries<any>(firstRow).map(([name, value]) => {
+      visiblePropertyEntries.map(([name, value]) => {
         let schema: any = {}
         if (typeof value === 'number') schema = { type: 'number' }
         return [name, schema]
