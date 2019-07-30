6c0a3df81aacd4885e27f9966b372632a8644ae4
Diego Mijelshon <diego@mijelshon.com.ar>
2019-07-29 22:50:15 -0300

    Better typing for local variable

--- packages/examples/grid/src/inferSchema.ts
+++ packages/examples/grid/src/inferSchema.ts
@@ -1,4 +1,4 @@
-import { JSONSchema7 } from 'json-schema'
+import { JSONSchema7, JSONSchema7Definition } from 'json-schema'
 import uuid from 'uuid'
 
 export function inferSchema(sampleData: any): JSONSchema7 {
@@ -11,8 +11,8 @@ export function inferSchema(sampleData: any): JSONSchema7 {
     type: 'object',
     properties: Object.fromEntries(
       visiblePropertyEntries.map(([name, value]) => {
-        let schema: any = {}
-        if (typeof value === 'number') schema = { type: 'number' }
+        let schema: JSONSchema7Definition = {}
+        if (typeof value === 'number') schema.type = 'number'
         return [name, schema]
       })
     ),
