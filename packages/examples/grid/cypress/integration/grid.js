1089e0e389fe4d27a5781653d5169cbbb76ea4ba
Herb Caudill <herb@devresults.com>
2019-08-03 13:46:48 +0200

    add cypress

---
+++ packages/examples/grid/cypress/integration/grid.js
@@ -0,0 +1,12 @@
+﻿describe('grid', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+  it('accepts input', () => {
+    cy.get('.ag-row-first > .ag-cell-range-left')
+      .click()
+      .type('123')
+
+    // cy.contains('.ag-root-wrapper')
+  })
+})
