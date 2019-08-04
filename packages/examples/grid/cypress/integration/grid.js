54f64667f6575466d1a469ae17e9e9bb6e3923c3
Herb Caudill <herb@devresults.com>
2019-08-04 19:11:27 +0200

    examples/grid/cypress/integration/grid.js:

--- packages/examples/grid/cypress/integration/grid.js
+++ packages/examples/grid/cypress/integration/grid.js
@@ -3,72 +3,61 @@
 const keycode = name => ({ keyCode: codes[name], which: codes[name] })
 
 describe('grid', () => {
-  beforeEach(() => {
-    cy.visit('/')
-    cy.get('.ag-cell:first').as('firstCell')
-    cy.get('.ag-center-cols-viewport .ag-row').as('rows')
-    cy.get('.ag-header-cell').as('cols')
-    cy.queryByText('New').as('newButton')
-    cy.queryByText('Join').as('joinButton')
-  })
+  beforeEach(() => cy.visit('/'))
 
   describe('on first load', () => {
     it('accepts input', () => {
-      cy.get('@firstCell')
-        .click()
-        .trigger('keydown', keycode('f2'))
-        .get('.ag-cell-focus input')
+      cy.firstCell()
+        .edit()
         .type('qrs')
         .type('{enter}')
-      cy.get('@firstCell').should('contain.text', 'qrs')
+      cy.firstCell().should('contain.text', 'qrs')
     })
 
     it('expands to the right', () => {
       // confirm that we start with 3 columns
-      cy.get('@cols').should('have.length', 3)
+      cy.columns().should('have.length', 3)
 
-      // start in first cell, hit right arrow three times
-      cy.get('@firstCell').type('{rightarrow}')
-      cy.get('.ag-cell-focus').type('{rightarrow}')
-      cy.get('.ag-cell-focus').type('{rightarrow}')
+      // start in first cell, hit right arrow 3 times
+      cy.firstCell()
+        .right()
+        .right()
+        .right()
 
       // confirm that we now have 4 columns
-      cy.get('@cols').should('have.length', 4)
+      cy.columns().should('have.length', 4)
     })
 
     it('expands downwards', () => {
       // confirm that we start with 3 rows
-      cy.get('@rows').should('have.length', 3)
+      cy.rows().should('have.length', 3)
 
-      // start in first cell, hit down arrow three times
-      cy.get('@firstCell').type('{downarrow}')
-      cy.get('.ag-cell-focus').type('{downarrow}')
-      cy.get('.ag-cell-focus').type('{downarrow}')
+      // start in first cell, hit down arrow 3 times
+      cy.firstCell()
+        .down()
+        .down()
+        .down()
 
-      // confirm that we now have four cells
-      cy.get('@rows').should('have.length', 4)
+      // confirm that we now have 4 cells
+      cy.rows().should('have.length', 4)
     })
   })
 
   describe('after creating store', () => {
     beforeEach(() => {
       // create a new document
-      cy.get('@newButton')
-        .click()
-        .wait(500) // give page time to reload
+      cy.clickNew()
     })
 
     it('accepts input', () => {
       // enter some text in the first cell, then hit enter
-      cy.get('@firstCell')
-        .click()
-        .trigger('keydown', keycode('f2'))
-        .get('.ag-cell-focus input')
+      cy.firstCell()
+        .edit()
         .type('qrs')
         .type('{enter}')
 
       // check that the text is in the cell
-      cy.get('@firstCell').should('contain.text', 'qrs')
+      cy.firstCell().should('contain.text', 'qrs')
     })
   })
 })
