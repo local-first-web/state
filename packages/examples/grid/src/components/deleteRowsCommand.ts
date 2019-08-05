9819332c7514c681acbc1f16ab39d2ca92ae4db4
Herb Caudill <herb@devresults.com>
2019-08-05 14:01:06 +0200

    examples/grid/components/deleteRowsCommand.ts: create

---
+++ packages/examples/grid/src/components/deleteRowsCommand.ts
@@ -0,0 +1,20 @@
+﻿import { GetContextMenuItemsParams } from 'ag-grid-community'
+import { getSelectedRowIds } from 'src/gridUtils'
+import { removeItem } from 'src/redux/actions'
+import { Dispatch, AnyAction } from 'redux'
+
+export const deleteRowsCommand = (
+  dispatch: Dispatch<AnyAction>,
+  { api, node }: GetContextMenuItemsParams
+) => {
+  if (!api) return
+
+  let rowIds: string[] = getSelectedRowIds(api, node)
+
+  const name = `Delete ${rowIds.length} ${rowIds.length === 1 ? 'row' : 'rows'}`
+
+  const deleteRow = (rowId: string) => dispatch(removeItem(rowId))
+  const action = () => rowIds.forEach(deleteRow)
+
+  return { name, action }
+}
