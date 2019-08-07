1acf62fbe6e46e18851f0c9925706871b96cc3e6
Herb Caudill <herb@devresults.com>
2019-08-07 14:53:49 +0200

    core/Cevitxe.ts: WIP watchableDoc->docSet

--- packages/core/src/Cevitxe.ts
+++ packages/core/src/Cevitxe.ts
@@ -1,19 +1,18 @@
-import A from 'automerge'
+import A, { Doc } from 'automerge'
+import { Client, newid, Peer } from 'cevitxe-signal-client'
 import debug from 'debug'
 import { EventEmitter } from 'events'
 import hypercore from 'hypercore'
 import db from 'random-access-idb'
 import * as Redux from 'redux'
 import { Middleware, Store } from 'redux'
-
-import { Client, Peer, newid } from 'cevitxe-signal-client'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { adaptReducer } from './adaptReducer'
 import { Connection } from './Connection'
 import { DEFAULT_SIGNAL_SERVERS } from './constants'
 import { getMiddleware } from './getMiddleware'
-import { getKeys, getKnownDocumentIds } from './keys'
-import { CevitxeOptions, ProxyReducer, StateFactory, ProxyMultiReducer } from './types'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { getKeys, getKnowndiscoveryKeys } from './keys'
+import { CevitxeOptions, ProxyMultiReducer, ProxyReducer } from './types'
 
 const valueEncoding = 'utf-8'
 
@@ -21,7 +20,7 @@ let log = debug('cevitxe')
 
 export class Cevitxe<T> extends EventEmitter {
   private proxyReducer: ProxyReducer<any> | ProxyMultiReducer
-  private initialState: T | StateFactory<T>
+  private initialState: T
   private urls: string[]
   private middlewares: Middleware[] // TODO: accept an `enhancer` object instead
 
@@ -31,7 +30,7 @@ export class Cevitxe<T> extends EventEmitter {
   public connections: { [peerId: string]: Connection }
   public databaseName: string
   public store?: Store
-  public documentId?: string = undefined
+  public discoveryKey?: string = undefined
 
   constructor({
     databaseName,
@@ -49,33 +48,38 @@ export class Cevitxe<T> extends EventEmitter {
     this.connections = {}
   }
 
-  joinStore = (documentId: string) => this.makeStore(documentId, false)
+  joinStore = (discoveryKey: string) => this.makeStore(discoveryKey, false)
 
-  createStore = (documentId: string) => this.makeStore(documentId, true)
+  createStore = (discoveryKey: string) => this.makeStore(discoveryKey, true)
 
-  private makeStore = async (documentId: string, creating: boolean = false) => {
-    log = debug(`cevitxe:${creating ? 'createStore' : 'joinStore'}:${documentId}`)
-    this.documentId = documentId
+  private makeStore = async (discoveryKey: string, creating: boolean = false) => {
+    log = debug(`cevitxe:${creating ? 'createStore' : 'joinStore'}:${discoveryKey}`)
+    this.discoveryKey = discoveryKey
 
-    this.feed = await createStorageFeed(documentId, this.databaseName)
+    this.feed = await createStorageFeed(discoveryKey, this.databaseName)
 
-    const state = creating
+    const state: any = creating
       ? setInitialState(this.feed, this.initialState) // ceating a new document, starting with default state
       : this.feed.length > 0
       ? await getStateFromStorage(this.feed) // rehydrating state from storage
       : setInitialState(this.feed, {}) // joining a peer's feed, starting with an empty doc
 
-    const watchableDoc = new A.WatchableDoc(state)
-    watchableDoc.registerHandler(doc => {
-      log('change', documentId)
+    const docSet = new A.DocSet<any>()
+    let key: string
+    for (key in Object.getOwnPropertyNames(state)) {
+      docSet.setDoc(key, state[key])
+    }
+
+    docSet.registerHandler(doc => {
+      log('change', discoveryKey)
       // hook for testing
       this.emit('change', doc)
     })
-    log('created initial watchableDoc', state)
+    log('created initial docSet', state)
 
     // Create Redux store
     const reducer = adaptReducer(this.proxyReducer)
-    const cevitxeMiddleware = getMiddleware(this.feed, watchableDoc, this.documentId)
+    const cevitxeMiddleware = getMiddleware(this.feed, docSet, this.discoveryKey)
     const enhancer = composeWithDevTools(
       Redux.applyMiddleware(...this.middlewares, cevitxeMiddleware)
     )
@@ -85,7 +89,7 @@ export class Cevitxe<T> extends EventEmitter {
     const url = this.urls[0]
     const client = new Client({ id: this.id, url })
 
-    client.join(documentId)
+    client.join(discoveryKey)
 
     // For each peer that wants to connect, create a Connection object
     client.on('peer', async (peer: Peer) => {
@@ -95,8 +99,8 @@ export class Cevitxe<T> extends EventEmitter {
 
       peer.on('close', () => this.removePeer(peer.id))
 
-      const socket = peer.get(documentId)
-      const connection = new Connection(watchableDoc, socket, this.store.dispatch)
+      const socket = peer.get(discoveryKey)
+      const connection = new Connection(docSet, socket, this.store.dispatch)
       this.connections[peer.id] = connection
       log('connected to peer', peer.id)
 
@@ -110,8 +114,8 @@ export class Cevitxe<T> extends EventEmitter {
     return Object.keys(this.connections).length
   }
 
-  get knownDocumentIds() {
-    return getKnownDocumentIds(this.databaseName)
+  get knowndiscoveryKeys() {
+    return getKnowndiscoveryKeys(this.databaseName)
   }
 
   removePeer = (peerId: string) => {
@@ -140,7 +144,7 @@ export class Cevitxe<T> extends EventEmitter {
   }
 }
 
-const getStateFromStorage = async (feed: Feed<string>) => {
+const getStateFromStorage = async <T>(feed: Feed<string>): Promise<Doc<T>> => {
   log('getting change sets from storage')
 
   // read full contents of the feed in one batch
@@ -149,18 +153,14 @@ const getStateFromStorage = async (feed: Feed<string>) => {
   const changeSets = data.map(changes => JSON.parse(changes))
 
   log('rehydrating from stored change sets %o', changeSets)
-  let state = A.init()
+  let state = A.init<T>()
   changeSets.forEach(changes => (state = A.applyChanges(state, changes)))
 
   log('done rehydrating')
   return state
 }
 
-const setInitialState = <T>(feed: Feed<string>, initialState: T | StateFactory<T>) => {
-  if (isFunction(initialState)) {
-    const generateInitialState = initialState as (StateFactory<T>)
-    initialState = generateInitialState()
-  }
+const setInitialState = <T>(feed: Feed<string>, initialState: T) => {
   log('nothing in storage; initializing %o', initialState)
 
   const state = A.from(initialState)
@@ -172,10 +172,10 @@ const setInitialState = <T>(feed: Feed<string>, initialState: T | StateFactory<T
   return state
 }
 
-const createStorageFeed = async (documentId: string, databaseName: string) => {
+const createStorageFeed = async (discoveryKey: string, databaseName: string) => {
   log('creating storage feed')
-  const { key, secretKey } = getKeys(databaseName, documentId)
-  const storage = db(`cevitxe-${databaseName}-${documentId.substr(0, 12)}`)
+  const { key, secretKey } = getKeys(databaseName, discoveryKey)
+  const storage = db(`cevitxe-${databaseName}-${discoveryKey.substr(0, 12)}`)
 
   const feed: Feed<string> = hypercore(storage, key, { secretKey, valueEncoding })
   feed.on('error', (err: any) => console.error(err))
