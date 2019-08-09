ad2cd9cce76bb40c221dc553b8ab5d8a1e36b55d
Herb Caudill <herb@devresults.com>
2019-08-09 14:51:38 +0200

    examples/grid/components/Toolbar.tsx: remove loading indicator; add MemoryStats

--- packages/examples/grid/src/components/Toolbar.tsx
+++ packages/examples/grid/src/components/Toolbar.tsx
@@ -11,20 +11,30 @@ export function Toolbar() {
     return state.list.length
   })
 
-  const loading = useSelector((state: State) => {
-    if (state === undefined || state.list === undefined) return 'Loading...'
-    return ''
-  })
-
   return (
     <div css={styles.toolbar}>
       <DataGenerator />
       <div css={styles.toolbarGroup}>
         <label>{rows} rows</label>
       </div>
-      <div css={styles.toolbarGroup}>
-        <label>{loading}</label>
-      </div>
+      <MemoryStats />
+    </div>
+  )
+}
+
+const MemoryStats = () => {
+  const percent = (n: number) => Math.floor(n * 100) + '%'
+  const mb = (n: number) => Math.floor(n / 1024 / 1024) + ' MB'
+
+  const { memory } = window.performance as any
+
+  if (!memory) return <div />
+  const { jsHeapSizeLimit: limit, totalJSHeapSize: allocated, usedJSHeapSize: used } = memory
+  return (
+    <div css={styles.toolbarGroup}>
+      <label>
+        JS heap used: {mb(used)} ({percent(used / limit)})
+      </label>
     </div>
   )
 }
