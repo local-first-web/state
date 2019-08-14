2979b03ca593deac0260183820f63afcd6ce7d9e
Herb Caudill <herb@devresults.com>
2019-08-14 17:19:11 +0200

    examples/grid/ag-grid/emptyGrid.test.ts: refactor

--- packages/examples/grid/src/ag-grid/emptyGrid.test.ts
+++ packages/examples/grid/src/ag-grid/emptyGrid.test.ts
@@ -1,15 +1,32 @@
 import { emptyGrid } from './emptyGrid'
 
 describe('emptyGrid', () => {
-  it('should ', () => {
-    expect(emptyGrid(2, 2)).toEqual({
-      '0': { id: '0' },
-      '1': { id: '1' },
-      index: { '0': true, '1': true },
+  it('2x2', () => {
+    expect(emptyGrid(2)).toEqual({
+      row_1: { id: 'row_1' },
+      row_2: { id: 'row_2' },
+      rows: ['row_1', 'row_2'],
       schema: {
         properties: {
-          '0': { description: 'Field 1' },
-          '1': { description: 'Field 2' },
+          col_1: { description: 'Field 1' },
+          col_2: { description: 'Field 2' },
+        },
+      },
+    })
+  })
+  it('5x3', () => {
+    expect(emptyGrid(5, 3)).toEqual({
+      row_1: { id: 'row_1' },
+      row_2: { id: 'row_2' },
+      row_3: { id: 'row_3' },
+      row_4: { id: 'row_4' },
+      row_5: { id: 'row_5' },
+      rows: ['row_1', 'row_2', 'row_3', 'row_4', 'row_5'],
+      schema: {
+        properties: {
+          col_1: { description: 'Field 1' },
+          col_2: { description: 'Field 2' },
+          col_3: { description: 'Field 3' },
         },
       },
     })
