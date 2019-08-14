4b6fb2a93406b1079815bcf6a50e915ed0e685e2
Herb Caudill <herb@devresults.com>
2019-08-14 17:25:41 +0200

    examples/grid/ag-grid/emptyGrid.test.ts:

--- packages/examples/grid/src/ag-grid/emptyGrid.test.ts
+++ packages/examples/grid/src/ag-grid/emptyGrid.test.ts
@@ -5,7 +5,10 @@ describe('emptyGrid', () => {
     expect(emptyGrid(2)).toEqual({
       row_1: { id: 'row_1' },
       row_2: { id: 'row_2' },
-      rows: ['row_1', 'row_2'],
+      rows: {
+        row_1: true, //
+        row_2: true,
+      },
       schema: {
         properties: {
           col_1: { description: 'Field 1' },
@@ -21,7 +24,13 @@ describe('emptyGrid', () => {
       row_3: { id: 'row_3' },
       row_4: { id: 'row_4' },
       row_5: { id: 'row_5' },
-      rows: ['row_1', 'row_2', 'row_3', 'row_4', 'row_5'],
+      rows: {
+        row_1: true, //
+        row_2: true,
+        row_3: true,
+        row_4: true,
+        row_5: true,
+      },
       schema: {
         properties: {
           col_1: { description: 'Field 1' },
