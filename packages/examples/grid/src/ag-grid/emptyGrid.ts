75a44de8a95944716d267d8dc644c20ccbfa037b
Herb Caudill <herb@devresults.com>
2019-08-14 17:19:07 +0200

    examples/grid/ag-grid/emptyGrid.ts: refactor

--- packages/examples/grid/src/ag-grid/emptyGrid.ts
+++ packages/examples/grid/src/ag-grid/emptyGrid.ts
@@ -1,32 +1,23 @@
 import { JSONSchema7 } from 'json-schema'
 import { State } from '../redux/store'
+import { range } from 'ramda'
 
-export const emptyGrid = (rowCount: number, colCount: number) => {
-  const range = (n: number): number[] => [...Array(n).keys()]
+export const emptyGrid = (rowCount: number, colCount: number = rowCount) => {
+  const rows = range(0, rowCount).map(i => `row_${i + 1}`)
+  const cols = range(0, colCount).map(i => `col_${i + 1}`)
 
-  const rows = range(rowCount).map(i => i.toString())
-  const cols = range(colCount).map(i => i.toString())
-
-  const rowReducer = (rowMap: State['map'], i: string) => ({
+  const rowReducer = (rowMap: State['map'], id: string, i: number) => ({
     ...rowMap,
-    [i]: { id: i },
+    [id]: { id },
   })
-  const columnReducer = (colMap: JSONSchema7['properties'], i: string) => ({
+  const columnReducer = (colMap: JSONSchema7['properties'], id: string, i: number) => ({
     ...colMap,
-    [i]: { description: `Field ${+i + 1}` },
+    [id]: { description: `Field ${+i + 1}` },
   })
 
   return {
     ...rows.reduce(rowReducer, {}),
-    index: Object.fromEntries(rows.map(d => [d, true])),
+    rows,
     schema: { properties: cols.reduce(columnReducer, {}) },
   }
 }
-
-// export interface State {
-//   // list: string[]
-//   // map: { [key: string]: any }
-//   [key: string]: any
-//   index: { [key: string]: boolean }
-//   schema: JSONSchema7
-// }
