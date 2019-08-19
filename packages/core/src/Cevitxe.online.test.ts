71157730a956be645ebb1f25d2d547ed605a6638
Herb Caudill <herb@devresults.com>
2019-08-19 13:46:24 +0200

    break Cevitxe tests into two files

--- packages/core/src/Cevitxe.online.test.ts
+++ packages/core/src/Cevitxe.online.test.ts
@@ -1,16 +1,15 @@
-import { Server } from 'cevitxe-signal-server'
-import { Cevitxe } from './Cevitxe'
 import { newid } from 'cevitxe-signal-client'
-import { ProxyReducer, ChangeMap } from './types'
-import { pause } from './lib/pause'
-import eventPromise from 'p-event'
+import { Server } from 'cevitxe-signal-server'
 import debug from 'debug'
-import { docSetToObject } from './docSetHelpers'
+import eventPromise from 'p-event'
 import { getPortPromise as getAvailablePort } from 'portfinder'
+import { Cevitxe } from './Cevitxe'
+import { ProxyReducer } from './types'
 require('fake-indexeddb/auto')
 
 const log = debug('cevitxe:test')
-describe('Cevitxe', () => {
+
+describe('Cevitxe (online)', () => {
   interface FooState {
     settings: {
       foo?: number
@@ -20,145 +19,32 @@ describe('Cevitxe', () => {
   const initialLocalState: FooState = { settings: { foo: 1 } }
   const initialRemoteState = {}
 
-  const proxyReducer: ProxyReducer = ({ type, payload }) => {
-    switch (type) {
-      case 'SET_FOO':
-        return {
-          settings: s => (s.foo = payload.value),
-        }
-      default:
-        return null
-    }
-  }
-
   const newDiscoveryKey = () => newid(6)
 
-  const getLocalCevitxe = (port: number = 1234) => {
-    const urls = [`ws://localhost:${port}`]
-    const databaseName = `local-${newid()}`
-    return new Cevitxe({ databaseName, proxyReducer, initialState: initialLocalState, urls })
-  }
+  describe('single document', () => {
+    const proxyReducer: ProxyReducer = ({ type, payload }) => {
+      switch (type) {
+        case 'SET_FOO':
+          return {
+            settings: s => (s.foo = payload.value),
+          }
+        default:
+          return null
+      }
+    }
 
-  const getRemoteCevitxe = (port: number = 1234) => {
-    const urls = [`ws://localhost:${port}`]
-    const databaseName = `remote-${newid()}`
-    return new Cevitxe({ databaseName, proxyReducer, initialState: initialRemoteState, urls })
-  }
+    const getLocalCevitxe = (port: number = 1234) => {
+      const urls = [`ws://localhost:${port}`]
+      const databaseName = `local-${newid()}`
+      return new Cevitxe({ databaseName, proxyReducer, initialState: initialLocalState, urls })
+    }
 
-  // TODO: Figure out why offline test suite causes "connected" suites to fail
-  describe('offline', () => {
-    describe('joinStore', () => {
-      it('should return a redux store with empty state', async () => {
-        expect.assertions(2)
+    const getRemoteCevitxe = (port: number = 1234) => {
+      const urls = [`ws://localhost:${port}`]
+      const databaseName = `remote-${newid()}`
+      return new Cevitxe({ databaseName, proxyReducer, initialState: initialRemoteState, urls })
+    }
 
-        const discoveryKey = newDiscoveryKey()
-        const localCevitxe = await getLocalCevitxe()
-        const localStore = await localCevitxe.joinStore(discoveryKey)
-
-        // store exists
-        expect(localStore).not.toBeUndefined()
-
-        // it's in empty (waiting) state
-        expect(localStore.getState()).toEqual({})
-
-        await pause(100) // HACK: wait for indexeddb to finish whatever it's doing
-        await localCevitxe.close()
-      })
-    })
-
-    describe('createStore', () => {
-      it('should return a redux store', async () => {
-        expect.assertions(5)
-        const discoveryKey = newDiscoveryKey()
-        const localCevitxe = await getLocalCevitxe()
-        const localStore = await localCevitxe.createStore(discoveryKey)
-
-        // store exists
-        expect(localStore).not.toBeUndefined()
-
-        // it looks like a store
-        expect(localStore).toHaveProperty('getState')
-        expect(localStore).toHaveProperty('dispatch')
-        expect(localStore).toHaveProperty('subscribe')
-
-        // it contains the initial state
-        expect(localStore.getState()).toEqual(initialLocalState)
-
-        await pause(100) // HACK: wait for indexeddb to finish whatever it's doing
-        await localCevitxe.close()
-      })
-
-      it('should use the reducer we gave it', async () => {
-        expect.assertions(1)
-
-        const discoveryKey = newDiscoveryKey()
-        const localCevitxe = await getLocalCevitxe()
-        const localStore = await localCevitxe.createStore(discoveryKey)
-
-        // dispatch a change
-        localStore.dispatch({ type: 'SET_FOO', payload: { value: 3 } })
-
-        // confirm that the change was made
-        const state = localStore.getState()
-        expect(state.settings.foo).toEqual(3)
-
-        await pause(100) // HACK: wait for indexeddb to finish whatever it's doing
-        await localCevitxe.close()
-      })
-    })
-
-    describe('close', () => {
-      it('should destroy any current store', async () => {
-        expect.assertions(2)
-
-        const discoveryKey = newDiscoveryKey()
-        const localCevitxe = await getLocalCevitxe()
-        const localStore = await localCevitxe.createStore(discoveryKey) // don't need return value
-
-        // confirm that we have a store
-        expect(localCevitxe.store).not.toBeUndefined()
-        await pause(100) // HACK for indexeddb
-
-        // close the store
-        await localCevitxe.close()
-
-        // confirm the store is gone
-        expect(localCevitxe.store).toBeUndefined()
-      })
-    })
-
-    describe('persistence', () => {
-      it('should rehydrate from persisted state when available', async () => {
-        const discoveryKey = newDiscoveryKey()
-        const localCevitxe = await getLocalCevitxe()
-        const localStore = await localCevitxe.createStore(discoveryKey)
-
-        const state = localStore.getState()
-        expect(state.settings.foo).toEqual(1)
-
-        // To simulate rehydrating from persisted state we dispatch a change to our local store.
-        // This state gets written to our fake-indexeddb.
-        localStore.dispatch({ type: 'SET_FOO', payload: { value: 42 } })
-
-        // confirm that the change took
-        const updatedState = localStore.getState()
-        expect(updatedState.settings.foo).toEqual(42)
-
-        // disconnect current store
-        await pause(500) // HACK:
-        await localCevitxe.close()
-
-        // Then we create a new store, which should see the state in the fake db and load it
-        const newStore = await localCevitxe.joinStore(discoveryKey)
-
-        // Confirm that the modified state is still there
-        const newState = newStore.getState()
-        expect(newState.settings.foo).toEqual(42)
-      })
-    })
-  })
-
-  describe('online', () => {
     let server: Server
 
     afterEach(async () => {
@@ -166,25 +52,29 @@ describe('Cevitxe', () => {
     })
 
     const open = async () => {
-      const port = await getAvailablePort({ port: 3000 })
+      // const port = await getAvailablePort({ port: 3000 })
+      const port = 3001
 
       server = new Server({ port })
       await server.listen({ silent: true })
-
+      log('server listening')
       const discoveryKey = newDiscoveryKey()
 
       // local cevitxe & store
       const localCevitxe = getLocalCevitxe(port)
       const localStore = await localCevitxe.createStore(discoveryKey)
+      log('localStore created')
 
       // remote cevitxe (tests control timing of joining store)
       const remoteCevitxe = getRemoteCevitxe(port)
 
       // join store from remote store
       const remoteStore = await remoteCevitxe.joinStore(discoveryKey)
+      log('remoteStore created')
 
       // wait for local peer to see connection
       await eventPromise(localCevitxe, 'peer')
+      log('localCevitxe sees peer')
 
       // include a teardown function in the return values
       const close = async () => {
@@ -199,20 +89,27 @@ describe('Cevitxe', () => {
       const { close, remoteCevitxe, localStore, remoteStore } = await open()
 
       // change something in the local store
-      localStore.dispatch({ type: 'SET_FOO', payload: { value: 42 } })
+      localStore.dispatch({
+        type: 'SET_FOO',
+        payload: {
+          value: 42,
+        },
+      })
+
+      // wait for remote peer to see change
+      await eventPromise(remoteCevitxe, 'change')
+      log('remoteCevitxe sees change')
 
       // confirm that the change took locally
       const localState = localStore.getState()
       expect(localState.settings.foo).toEqual(42)
-
-      // wait for remote peer to see change
-      await eventPromise(remoteCevitxe, 'change')
 
       // confirm that the remote store has the new value
       const remoteState = remoteStore.getState()
       expect(remoteState.settings.foo).toEqual(42)
 
       await close()
+      log('closed')
     })
 
     describe('close', () => {
@@ -241,6 +138,7 @@ describe('Cevitxe', () => {
       teachers: {}
       [k: string]: any
     }
+
     const proxyReducer = (({ type, payload }) => {
       switch (type) {
         case 'ADD_TEACHER': {
@@ -345,7 +243,10 @@ describe('Cevitxe', () => {
       await eventPromise(remoteCevitxe, 'change')
 
       // modify the teacher in the remote store
-      remoteStore.dispatch({ type: 'UPDATE_TEACHER', payload: { id: 'abcxyz', email: 'h@hc3.me' } })
+      remoteStore.dispatch({
+        type: 'UPDATE_TEACHER',
+        payload: { id: 'abcxyz', email: 'h@hc3.me' },
+      })
       await eventPromise(localCevitxe, 'change')
 
       // modify the teacher in the local store
