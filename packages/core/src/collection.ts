7b8fb319241449e3abf225ffa92d1863856972ea
Herb Caudill <herb@devresults.com>
2019-08-20 15:22:28 +0200

    core/collection.ts: refactor

--- packages/core/src/collection.ts
+++ packages/core/src/collection.ts
@@ -1,33 +1,52 @@
-import A from 'automerge'
 import { DocSet } from './lib/automerge'
 import { DELETE_COLLECTION, DELETE_ITEM } from './constants'
 
-const collKey = (key: string) => `__col_${key}`
-const itmKey = (key: string, itemKey: string) => `${collKey(key)}_${itemKey}`
+interface CollectionOptions {
+  idField?: string
+}
 
-export const collection = (key: string) => ({
-  add: () => ({ [collKey(key)]: (s: any) => Object.assign(s, {}) }),
-  remove: () => {
-    return { [collKey(key)]: DELETE_COLLECTION }
-  },
-  addItem: (item: any, keyField: string = 'id') => ({
-    [collKey(key)]: (s: any) => Object.assign(s, { [itmKey(key, item[keyField])]: true }),
-    [itmKey(key, item[keyField])]: (s: any) => Object.assign(s, item),
-  }),
-  updateItem: (item: any, keyField: string = 'id') => ({
-    [itmKey(key, item[keyField])]: (s: any) => Object.assign(s, item),
-  }),
-  removeItem: (itemKey: string) => ({
-    [collKey(key)]: (s: any) => delete s[itmKey(key, itemKey)],
-    [itmKey(key, itemKey)]: DELETE_ITEM,
-  }),
-})
+/**
+ * The collection function returns helpers for CRUD operations, hiding some of the messiness of
+ * multi-document state from the developer.
+ *
+ * Each method (`add`, `remove`, etc.) returns a dictionary of ProxyReducer functions for modifying
+ * the collection index and/or the individual item.
+ *
+ * @param name The name of the collection.
+ * @param idField The property of an object containing a unique identifier, normally a uuid.
+ * Optional; defaults to 'id'.
+ */
+export function collection(name: string, { idField = 'id' }: CollectionOptions = {}) {
+  const collectionKey = `::${name}`
+
+  return {
+    drop: () => {
+      return { [collectionKey]: DELETE_COLLECTION }
+    },
+
+    add: (item: any) => ({
+      [collectionKey]: (s: any) => Object.assign(s, { [item[idField]]: true }),
+      [item[idField]]: (s: any) => Object.assign(s, item),
+    }),
+
+    update: (item: any) => ({
+      [item[idField]]: (s: any) => Object.assign(s, item),
+    }),
+
+    remove: (item: any) => ({
+      [collectionKey]: (s: any) => delete s[item[idField]],
+      [item[idField]]: DELETE_ITEM,
+    }),
+  }
+}
 
 export const deleteCollectionItems = (docSet: DocSet<any>, key: string) => {
   const collectionIndexDoc = docSet.getDoc(key)
+
   for (const docId in collectionIndexDoc) {
     docSet.removeDoc(docId)
     delete collectionIndexDoc[docId]
   }
+
   docSet.setDoc(key, collectionIndexDoc)
 }
