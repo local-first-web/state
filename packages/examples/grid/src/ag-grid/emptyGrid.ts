c090ab33a1bda41b3b13d8e8f77bdcf55c9e30b3
Herb Caudill <herb@devresults.com>
2019-08-21 12:02:34 +0200

    emptyGrid.ts: hard-code rowCollectionKey the import from cevitxe was causing Jest to throw an unrecognized token error, so just hard-coded this for now

--- packages/examples/grid/src/ag-grid/emptyGrid.ts
+++ packages/examples/grid/src/ag-grid/emptyGrid.ts
@@ -1,16 +1,17 @@
 import { JSONSchema7 } from 'json-schema'
 import { range } from 'ramda'
-import { rowCollectionKey } from '../redux/store'
+
+const rowCollectionKey = '::rows'
 
 export const emptyGrid = (rowCount: number, colCount: number = rowCount) => {
   const rows = range(0, rowCount).map(i => `row_${i + 1}`)
   const cols = range(0, colCount).map(i => `col_${i + 1}`)
 
-  const rowReducer = (rowMap: { [key: string]: any }, id: string) => ({
+  const rowReducer = (rowMap: { [id: string]: any }, id: string) => ({
     ...rowMap,
     [id]: { id },
   })
-  const rowIndexReducer = (rowIndex: any, id: string) => ({
+  const rowIndexReducer = (rowIndex: { [id: string]: boolean }, id: string) => ({
     ...rowIndex,
     [id]: true,
   })
