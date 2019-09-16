1c3d92088dd5ea7e57eef0a111580ac065029e92
Herb Caudill <herb@devresults.com>
2019-09-16 16:59:44 +0200

    examples/grid/workers/generator.worker.ts:

--- packages/examples/grid/src/workers/generator.worker.ts
+++ packages/examples/grid/src/workers/generator.worker.ts
@@ -3,34 +3,8 @@ import faker from 'faker'
 
 declare const self: Worker
 
-// async function* generate(rows: number) {
-//   const collection = {} as any
-
-//   for (let i = 0; i < rows; i++) {
-//     const item = {
-//       id: uuid(),
-//       name: faker.name.findName(),
-//       email: faker.internet.email(),
-//       age: faker.random.number({ min: 18, max: 100 }),
-//       street: faker.address.streetAddress(),
-//       city: faker.address.city(),
-//       state: faker.address.state(),
-//       zip: faker.address.zipCode(),
-//       gender: faker.random.arrayElement(['Male', 'Female']),
-//       latitude: +faker.address.latitude(),
-//       longitude: +faker.address.longitude(),
-//       paragraph: faker.lorem.paragraph(),
-//     }
-//     collection[item.id] = item
-//     yield { progress: i }
-//   }
-//   return { result: collection }
-// }
-
 self.addEventListener('message', async e => {
-  // const progress = generate(+e.data)
-  // for await (const i of progress) self.postMessage(i)
-  const collection = {} as any
+  const collection = [] as any[]
   const rows = +e.data
   for (let i = 0; i < rows; i++) {
     const item = {
@@ -47,7 +21,7 @@ self.addEventListener('message', async e => {
       longitude: +faker.address.longitude(),
       paragraph: faker.lorem.paragraph(),
     }
-    collection[item.id] = item
+    collection.push(item)
     self.postMessage({ progress: i })
   }
   self.postMessage({ result: collection })
