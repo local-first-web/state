eef745f651b0ffdccf5df815a4d7370d7756b0ef
Herb Caudill <herb@devresults.com>
2019-09-17 19:10:29 +0200

    update emptyGrid

--- packages/examples/grid/src/ag-grid/emptyGrid.test.ts
+++ packages/examples/grid/src/ag-grid/emptyGrid.test.ts
@@ -1,42 +1,36 @@
 import { emptyGrid } from './emptyGrid'
+import { collection } from 'cevitxe'
 
 describe('emptyGrid', () => {
   it('2x2', () => {
-    expect(emptyGrid(2)).toEqual({
-      row_1: { id: 'row_1' },
-      row_2: { id: 'row_2' },
-      ['::rows']: {
-        row_1: true, //
-        row_2: true,
-      },
-      schema: {
-        properties: {
-          col_1: { description: 'Field 1' },
-          col_2: { description: 'Field 2' },
-        },
+    const state = emptyGrid(2)
+    const rows = collection('rows')
+    const allRows = rows.getAll(state)
+    expect(allRows).toEqual([{ id: 'row_1' }, { id: 'row_2' }])
+    expect(state.schema).toEqual({
+      properties: {
+        col_1: { description: 'Field 1' },
+        col_2: { description: 'Field 2' },
       },
     })
   })
+
   it('5x3', () => {
-    expect(emptyGrid(5, 3)).toEqual({
-      row_1: { id: 'row_1' },
-      row_2: { id: 'row_2' },
-      row_3: { id: 'row_3' },
-      row_4: { id: 'row_4' },
-      row_5: { id: 'row_5' },
-      ['::rows']: {
-        row_1: true, //
-        row_2: true,
-        row_3: true,
-        row_4: true,
-        row_5: true,
-      },
-      schema: {
-        properties: {
-          col_1: { description: 'Field 1' },
-          col_2: { description: 'Field 2' },
-          col_3: { description: 'Field 3' },
-        },
+    const state = emptyGrid(5, 3)
+    const rows = collection('rows')
+    const allRows = rows.getAll(state)
+    expect(allRows).toEqual([
+      { id: 'row_1' },
+      { id: 'row_2' },
+      { id: 'row_3' },
+      { id: 'row_4' },
+      { id: 'row_5' },
+    ])
+    expect(state.schema).toEqual({
+      properties: {
+        col_1: { description: 'Field 1' },
+        col_2: { description: 'Field 2' },
+        col_3: { description: 'Field 3' },
       },
     })
   })
