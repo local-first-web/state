7e2350c7456ccbd8e2a5deb9b47bc6bd7a358745
Herb Caudill <herb@devresults.com>
2019-09-20 14:51:48 +0200

    core/adaptReducer.ts: refactor

--- packages/core/src/adaptReducer.ts
+++ packages/core/src/adaptReducer.ts
@@ -1,51 +1,55 @@
 import A, { ChangeFn } from 'automerge'
-import debug from 'debug'
 import { RECEIVE_MESSAGE_FROM_PEER } from './constants'
 import { ReducerConverter } from './types'
 import { docSetToObject } from './docSetHelpers'
-import { collection } from './collection'
+import { Reducer } from 'react'
+import { AnyAction } from 'redux'
 
-import { DELETE_COLLECTION, DELETE_ITEM } from './constants'
+/**
+ * This function, used when wiring up the store, takes a `proxyReducer` and turns it into a
+ * garden-variety Redux reducer.
+ *
+ * @remarks
+ * When we initialize a `StoreManager`, we give it a `proxyReducer`, which is like a Redux reducer,
+ * except it's designed to work with Automerge objects instead of plain JavaScript objects. Instead
+ * of returning a modified state, it returns one or more change functions.
+ *
+ * @param proxyReducer The proxyReducer to be adapted
+ * @param docSet The store's docSet
+ */
+export const adaptReducer: ReducerConverter = (proxyReducer, docSet) => {
+  const reducer: Reducer<any, AnyAction> = (state, { type, payload }) => {
+    if (type === RECEIVE_MESSAGE_FROM_PEER) {
+      // Connection has already updated our docSet
+    } else {
+      const functionMap = proxyReducer({ type, payload })
 
-const log = debug('cevitxe:adaptReducer')
+      if (!functionMap || !state) {
+        // no matching function - return the unmodified state
+        return state
+      }
 
-// This function, used when wiring up the store, takes a proxyReducer and turns it into a real one.
-export const adaptReducer: ReducerConverter = (proxyReducer, docSet) => (state, action) => {
-  state = convertToReduxReducer(proxyReducer, docSet)(state, action)
-  return state
-}
+      // Apply each change function to the corresponding document
+      for (let docId in functionMap) {
+        const fn = functionMap[docId] as ChangeFn<any> | symbol
 
-// During initialization, we're given a `proxyReducer`, which is like a Redux reducer,
-// except it's designed to work with automerge objects instead of plain javascript objects.
-// Instead of returning a modified state, it returns change functions.
-
-// Also, when it doesn't find a reducer for a given action, it returns `null` instead of the previous state.
-
-// The purpose of this function is to turn a proxyReducer into a real reducer by
-// running the proxyReducer's change functions through `automerge.change`.
-const convertToReduxReducer: ReducerConverter = (proxyReducer, docSet) => (
-  state,
-  { type, payload }
-) => {
-  // Connection has already updated our docSet, update redux state to match
-  if (type === RECEIVE_MESSAGE_FROM_PEER) return docSetToObject(docSet)
-
-  const functionMap = proxyReducer({ type, payload })
-  if (!functionMap || !state) return state // no matching function - return the unmodified state
-  let docId: string
-  for (docId in functionMap) {
-    const fn = functionMap[docId] as ChangeFn<any> | symbol
-    if (fn === DELETE_COLLECTION) {
-      // docSet.removeDoc(docId)
-    } else if (fn === DELETE_ITEM) {
-      // docSet.removeDoc(docId)
-    } else if (typeof fn === 'function') {
-      // apply changes to the corresponding doc in the docset
-      const oldDoc = docSet.getDoc(docId) || A.init() // create a new doc if one doesn't exist
-      const newDoc = A.change(oldDoc, fn as ChangeFn<any>)
-      docSet.setDoc(docId, newDoc)
+        if (typeof fn === 'symbol') {
+          // instead of a change function, we got a symbol (e.g. DELETE_ITEM or DELETE_COLLECTION),
+          // in which case we'll do the work in middleware
+        } else {
+          // find the corresponding document in the docSet
+          const oldDoc = docSet.getDoc(docId) || A.init() // create a new doc if one doesn't exist
+          // run the change function to get a new document
+          const newDoc = A.change(oldDoc, fn)
+          // update the docSet
+          docSet.setDoc(docId, newDoc)
+        }
+      }
     }
+
+    const newState = docSetToObject(docSet)
+    return newState
   }
-  // return the new state of the docSet
-  return docSetToObject(docSet)
+
+  return reducer
 }
