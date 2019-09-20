dc21f48a851514c5daf185730283d01093948b7c
Herb Caudill <herb@devresults.com>
2019-09-20 14:53:22 +0200

    core/getMiddleware.ts: refactor

--- packages/core/src/getMiddleware.ts
+++ packages/core/src/getMiddleware.ts
@@ -2,20 +2,16 @@ import A from 'automerge'
 import debug from 'debug'
 import { collection } from './collection'
 import { DELETE_COLLECTION, DELETE_ITEM, RECEIVE_MESSAGE_FROM_PEER } from './constants'
-import { MiddlewareFactory } from './types'
+import { ChangeSet, MiddlewareFactory, State } from './types'
 
 const log = debug('cevitxe:middleware')
-
-interface DocMap {
-  [key: string]: A.Doc<any>
-}
 
 export const getMiddleware: MiddlewareFactory = (feed, docSet, proxyReducer) => {
   return store => next => action => {
     // BEFORE CHANGES
 
     // detect which documents will be changed and cache them
-    const affectedDocs: DocMap = {} // cache for docs that will be changed
+    const affectedDocs: State = {} // cache for docs that will be changed
     const removedDocs: string[] = [] // list of docs that will be removed
 
     const functionMap = proxyReducer(action)
@@ -24,13 +20,14 @@ export const getMiddleware: MiddlewareFactory = (feed, docSet, proxyReducer) => 
         const fn = functionMap[docId]
         if (fn === DELETE_COLLECTION) {
           const name = collection.getCollectionName(docId)
-          const docIds = collection(name).getKeys(store.getState())
+          const docIds = collection(name).selectors.keys(store.getState())
           for (const itemDocId in docIds) removedDocs.push(itemDocId)
           collection(name).deleteAll(docSet)
         } else if (fn === DELETE_ITEM) {
           removedDocs.push(docId)
         } else if (typeof fn === 'function') {
-          affectedDocs[docId] = docSet.getDoc(docId) || A.init() // If doc didn't exist before, it's a new doc
+          const oldDoc = docSet.getDoc(docId) || A.init() // create a new doc if one doesn't exist
+          affectedDocs[docId] = oldDoc
         }
       }
     }
@@ -44,7 +41,8 @@ export const getMiddleware: MiddlewareFactory = (feed, docSet, proxyReducer) => 
     log('%o', { action })
 
     // collect document changes for persistence
-    const changeSets = []
+    const changeSets: ChangeSet[] = []
+
     if (action.type === RECEIVE_MESSAGE_FROM_PEER) {
       // for changes coming from peer, we already have the Automerge changes, so just persist them
       const { docId, changes } = action.payload.message
@@ -58,7 +56,14 @@ export const getMiddleware: MiddlewareFactory = (feed, docSet, proxyReducer) => 
         if (changes.length > 0) changeSets.push({ docId, changes })
       }
       // for remove actions, we've made a list, so we just add a flag for each
-      for (const docId of removedDocs) changeSets.push({ docId, changes: [], isDelete: true })
+      for (const docId of removedDocs) {
+        const changeSet: ChangeSet = {
+          docId,
+          changes: [],
+          isDelete: true,
+        }
+        changeSets.push(changeSet)
+      }
     }
 
     // write any changes to the feed
