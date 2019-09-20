0c7bb5b375bddf3f5f778b4cc57cef3128f69b47
Herb Caudill <herb@devresults.com>
2019-09-20 14:54:21 +0200

    core/StoreManager: refactor StorageFeed into its own class

---
+++ packages/core/src/StorageFeed.ts
@@ -0,0 +1,95 @@
+﻿import A from 'automerge'
+import debug from 'debug'
+import { EventEmitter } from 'events'
+import hypercore from 'hypercore'
+import db from 'random-access-idb'
+import { getKeys } from './keys'
+import { ChangeSet } from './types'
+
+let log = debug('cevitxe:StorageFeed')
+
+export class StorageFeed extends EventEmitter {
+  private discoveryKey: string
+  private databaseName: string
+  private feed: Feed<string>
+  private isReady: boolean = false
+
+  public docSet: A.DocSet<any> = new A.DocSet()
+
+  constructor(discoveryKey: string, databaseName: string) {
+    super()
+    this.discoveryKey = discoveryKey
+    this.databaseName = databaseName
+    log('creating storage feed')
+    const { key, secretKey } = getKeys(this.databaseName, this.discoveryKey)
+    const storage = db(`cevitxe-${this.databaseName}-${this.discoveryKey.substr(0, 12)}`)
+
+    this.feed = hypercore(storage, key, { secretKey, valueEncoding: 'utf-8' })
+    this.feed.on('error', (err: any) => console.error(err))
+  }
+
+  init = (initialState: any): Promise<A.DocSet<any>> =>
+    new Promise(resolve =>
+      this.feed.on('ready', async () => {
+        const creating = Object.keys(initialState).length > 0
+        if (!creating && this.feed.length > 0) await this.getStateFromStorage()
+        else this.initialize(initialState)
+        this.isReady = true
+        log('ready!')
+        this.emit('ready')
+        resolve(this.docSet)
+      })
+    )
+
+  ready = async () => {
+    log('isReady', this.isReady)
+    return new Promise(ok => this.feed.on('ready', ok))
+  }
+
+  close = (cb: (err: Error) => void) =>
+    this.feed.close(err => {
+      cb(err)
+      this.isReady = false
+      this.emit('close')
+    })
+
+  append = (data: string) => this.feed.append(data)
+
+  private initialize(initialState: any) {
+    log('initializing %o', initialState)
+    const changeSets: ChangeSet[] = []
+    for (let docId in initialState) {
+      const doc = A.from(initialState[docId])
+      this.docSet.setDoc(docId, doc)
+      const changes = A.getChanges(A.init(), doc)
+      changeSets.push({ docId, changes })
+    }
+    this.feed.append(JSON.stringify(changeSets))
+  }
+
+  private async readAll() {
+    return new Promise<string[]>(ok =>
+      this.feed.getBatch(0, this.feed.length, (_, data) => ok(data))
+    )
+  }
+
+  private async getStateFromStorage() {
+    log('getting change sets from storage')
+
+    // read full contents of the feed in one batch
+    const feedContents = await this.readAll()
+
+    // each feed entry is the result of a single redux action, and contains one or more ChangeSets
+    const feedEntries: ChangeSet[][] = feedContents.map(s => JSON.parse(s))
+
+    log('rehydrating from stored change sets %o', feedEntries)
+    feedEntries.forEach(entry => {
+      entry.forEach(({ docId, changes, isDelete }) => {
+        if (isDelete) this.docSet.removeDoc(docId)
+        else this.docSet.applyChanges(docId, changes)
+      })
+    })
+
+    log('done rehydrating')
+  }
+}
--- packages/core/src/StoreManager.ts
+++ packages/core/src/StoreManager.ts
@@ -1,42 +1,40 @@
-import A from 'automerge'
-import { DocSet } from './lib/automerge'
 import { Client, newid, Peer } from 'cevitxe-signal-client'
 import debug from 'debug'
 import { EventEmitter } from 'events'
-import hypercore from 'hypercore'
-import db from 'random-access-idb'
-import * as Redux from 'redux'
-import { Middleware, Store } from 'redux'
+import * as A from 'automerge'
+import { Middleware, Store, createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { adaptReducer } from './adaptReducer'
 import { Connection } from './Connection'
 import { DEFAULT_SIGNAL_SERVERS } from './constants'
+import { docSetToObject } from './docSetHelpers'
 import { getMiddleware } from './getMiddleware'
-import { getKeys, getKnownDiscoveryKeys } from './keys'
-import { StoreManagerOptions, ProxyReducer } from './types'
-import { docSetFromObject, docSetToObject } from './docSetHelpers'
+import { getKnownDiscoveryKeys } from './keys'
+import { StorageFeed } from './StorageFeed'
+import { ProxyReducer, StoreManagerOptions } from './types'
 
-const valueEncoding = 'utf-8'
-
-let log = debug('cevitxe')
+let log = debug('cevitxe:StoreManager')
 
 // It's normal for a document with a lot of participants to have a lot of connections, so increase
 // the limit to avoid spurious warnings about emitter leaks.
 EventEmitter.defaultMaxListeners = 500
 
+/**
+ * A StoreManager
+ */
 export class StoreManager<T> extends EventEmitter {
   private proxyReducer: ProxyReducer
   private initialState: T
   private urls: string[]
   private middlewares: Middleware[] // TODO: accept an `enhancer` object instead
 
-  private id = newid()
-  private feed?: Feed<string>
+  private clientId = newid()
+  private feed?: StorageFeed
+  private docSet?: A.DocSet<T>
 
   public connections: { [peerId: string]: Connection }
   public databaseName: string
   public store?: Store
-  public discoveryKey?: string = undefined
 
   constructor({
     databaseName,
@@ -60,54 +58,23 @@ export class StoreManager<T> extends EventEmitter {
 
   private makeStore = async (discoveryKey: string, creating: boolean = false) => {
     log = debug(`cevitxe:${creating ? 'createStore' : 'joinStore'}:${discoveryKey}`)
-    this.discoveryKey = discoveryKey
 
-    this.feed = await createStorageFeed(discoveryKey, this.databaseName)
+    this.feed = new StorageFeed(discoveryKey, this.databaseName)
 
-    const docSet: DocSet<any> = creating
-      ? setInitialState(this.feed, this.initialState) // creating a new document, starting with default state
-      : this.feed.length > 0
-      ? await getStateFromStorage(this.feed) // rehydrating state from storage
-      : setInitialState(this.feed, {}) // joining a peer's feed, starting with an empty doc
-
-    const state = docSetToObject(docSet)
-
-    docSet.registerHandler((key, doc) => {
-      log('change', key)
-      // hook for testing
-      this.emit('change', key, doc)
-    })
-    log('created initial docSet', state)
+    this.docSet = await this.feed.init(this.initialState)
+    this.docSet.registerHandler(this.onChange)
 
     // Create Redux store
-    const reducer = adaptReducer(this.proxyReducer, docSet)
-    const cevitxeMiddleware = getMiddleware(this.feed, docSet, this.proxyReducer, this.discoveryKey)
-    const enhancer = composeWithDevTools(
-      Redux.applyMiddleware(...this.middlewares, cevitxeMiddleware)
-    )
-    this.store = Redux.createStore(reducer, state, enhancer)
+    const state = docSetToObject(this.docSet)
+    const reducer = adaptReducer(this.proxyReducer, this.docSet)
+    const cevitxeMiddleware = getMiddleware(this.feed, this.docSet, this.proxyReducer)
+    const enhancer = composeWithDevTools(applyMiddleware(...this.middlewares, cevitxeMiddleware))
+    this.store = createStore(reducer, state, enhancer)
 
-    // TODO: randomly select a URL if more than one is provided? select best based on ping?
-    const url = this.urls[0]
-    const client = new Client({ id: this.id, url })
-
+    // Connect to discovery server
+    const client = new Client({ id: this.clientId, url: this.urls[0] }) // TODO: randomly select a URL if more than one is provided? select best based on ping?
     client.join(discoveryKey)
-
-    // For each peer that wants to connect, create a Connection object
-    client.on('peer', async (peer: Peer) => {
-      if (this.store === undefined) return
-
-      log('connecting to peer', peer.id)
-
-      peer.on('close', () => this.removePeer(peer.id))
-
-      const socket = peer.get(discoveryKey)
-      const connection = new Connection(docSet, socket, this.store.dispatch)
-      this.connections[peer.id] = connection
-      log('connected to peer', peer.id)
-
-      this.emit('peer', peer) // hook for testing
-    })
+    client.on('peer', (peer: Peer) => this.addPeer(peer, discoveryKey))
 
     this.emit('ready', this.store)
     return this.store
@@ -117,18 +84,35 @@ export class StoreManager<T> extends EventEmitter {
     return Object.keys(this.connections).length
   }
 
-  get knownDiscoveryKeys() {
+  public get knownDiscoveryKeys() {
     return getKnownDiscoveryKeys(this.databaseName)
   }
 
-  removePeer = (peerId: string) => {
+  private onChange = (docId: string, doc: A.Doc<T>) => {
+    this.emit('change', docId, doc)
+  }
+
+  private addPeer = (peer: Peer, discoveryKey: string) => {
+    if (!this.store || !this.docSet) return
+    log('connecting to peer', peer.id)
+
+    // For each peer that wants to connect, create a Connection object
+    const socket = peer.get(discoveryKey)
+    const connection = new Connection(this.docSet, socket, this.store.dispatch)
+    this.connections[peer.id] = connection
+    this.emit('peer', peer) // hook for testing
+    log('connected to peer', peer.id)
+
+    peer.on('close', () => this.removePeer(peer.id))
+  }
+
+  private removePeer = (peerId: string) => {
     log('removing peer', peerId)
     if (this.connections[peerId]) this.connections[peerId].close()
     delete this.connections[peerId]
   }
 
   close = async () => {
-    this.store = undefined
     this.removeAllListeners()
 
     const closeAllConnections = Object.keys(this.connections).map(peerId => this.removePeer(peerId))
@@ -136,65 +120,15 @@ export class StoreManager<T> extends EventEmitter {
     this.connections = {}
 
     const feed = this.feed
-    if (feed) {
+    if (feed)
       await Promise.all([
         new Promise(ok => feed.close(ok)),
         new Promise(ok => feed.on('close', ok)),
       ])
-      this.feed = undefined
-    }
+
+    delete this.feed
+    delete this.store
+
     this.emit('close')
   }
-}
-
-const getStateFromStorage = async (feed: Feed<string>): Promise<DocSet<any>> => {
-  log('getting change sets from storage')
-
-  // read full contents of the feed in one batch
-  const feedContents = new Promise(yes => feed.getBatch(0, feed.length, (_, data) => yes(data)))
-  const data = (await feedContents) as string[]
-  const feedEntries = data.map(changes => JSON.parse(changes))
-
-  log('rehydrating from stored change sets %o', feedEntries)
-  let docSet = new DocSet()
-  feedEntries.forEach(entry => {
-    entry.forEach((changeSet: any) => {
-      if (changeSet.isDelete) {
-        docSet.removeDoc(changeSet.docId)
-      } else {
-        docSet.applyChanges(changeSet.docId, changeSet.changes)
-      }
-    })
-  })
-
-  log('done rehydrating')
-  return docSet
-}
-
-const setInitialState = <T>(feed: Feed<string>, initialState: T) => {
-  log('nothing in storage; initializing %o', initialState)
-
-  const docSet = docSetFromObject(initialState)
-  let changes = []
-  for (let docId of docSet.docIds) {
-    const nextDoc = docSet.getDoc(docId)
-    changes.push({
-      docId,
-      changes: A.getChanges(A.init(), nextDoc),
-    })
-  }
-  feed.append(JSON.stringify(changes))
-  return docSet
-}
-
-const createStorageFeed = async (discoveryKey: string, databaseName: string) => {
-  log('creating storage feed')
-  const { key, secretKey } = getKeys(databaseName, discoveryKey)
-  const storage = db(`cevitxe-${databaseName}-${discoveryKey.substr(0, 12)}`)
-
-  const feed: Feed<string> = hypercore(storage, key, { secretKey, valueEncoding })
-  feed.on('error', (err: any) => console.error(err))
-  await new Promise(ok => feed.on('ready', ok))
-  log('feed ready')
-  return feed
 }
