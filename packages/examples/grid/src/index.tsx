689d7a88ab1414fad0fce49f60dc9609c9f9f29d
Herb Caudill <herb@devresults.com>
2019-10-01 12:24:53 +0200

    remove unused service worker stuff to prevent confusion

--- packages/examples/grid/src/index.tsx
+++ packages/examples/grid/src/index.tsx
@@ -2,7 +2,6 @@ import 'core-js'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import * as serviceWorker from './serviceWorker'
 
 import { Shell } from './components/Shell'
 
@@ -11,8 +10,3 @@ const start = async () => {
   ReactDOM.render(<Shell />, rootElement)
 }
 start()
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
