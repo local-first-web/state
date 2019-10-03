f357cb24aa36a4c84169a7ed9e74bf67c867c58d
Diego Mijelshon <diego@mijelshon.com.ar>
2019-10-03 10:10:36 -0300

    Renamed StorageFeed -> Repo

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,7 +1,7 @@
 import A from 'automerge'
 import { AnyAction, Middleware, Reducer, Store } from 'redux'
 import { DocSetSync } from './DocSetSync'
-import { StorageFeed } from './StorageFeed'
+import { Repo } from './Repo'
 
 export type ProxyReducer = (state: any, action: AnyAction) => ChangeMap | null
 
@@ -28,7 +28,7 @@ export interface CreateStoreResult {
 }
 
 export type MiddlewareFactory = (
-  feed: StorageFeed,
+  feed: Repo,
   docSet: A.DocSet<any>,
   proxyReducer: ProxyReducer,
   discoveryKey?: string
