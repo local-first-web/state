d8ec4add2b6849a6ddd1dd03d053c30924021563
Herb Caudill <herb@devresults.com>
2019-10-06 17:57:16 +0200

    core/lib/pause.ts: add logging

--- packages/core/src/lib/pause.ts
+++ packages/core/src/lib/pause.ts
@@ -1,1 +1,11 @@
-export const pause = (t = 100) => new Promise(ok => setTimeout(ok, t));
+import debug from 'debug'
+const log = debug(`cevitxe:pause`)
+export const pause = (t = 100) => {
+  log('start')
+  return new Promise(resolve =>
+    setTimeout(() => {
+      log('end')
+      resolve()
+    }, t)
+  )
+}
