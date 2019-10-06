79151da6aecda1df6cdd1beb9f38873320a44092
Herb Caudill <herb@devresults.com>
2019-10-06 17:57:38 +0200

    core/lib/TestChannel.ts: document to remind myself

--- packages/core/src/lib/TestChannel.ts
+++ packages/core/src/lib/TestChannel.ts
@@ -1,5 +1,18 @@
 ﻿import { EventEmitter } from 'events'
 import { Message } from '../types'
+
+/**
+ * Simplest possible communications channel, for testing.
+ *
+ * Example:
+ * ```ts
+ * const channel = new TestChannel()
+ * channel.on('data', msg => {
+ *   console.log(msg)
+ * })
+ * channel.write('hello, world') // logs 'hello, world'
+ * ```
+ */
 export class TestChannel extends EventEmitter {
   write(id: string, msg: Message) {
     this.emit('data', id, msg)
