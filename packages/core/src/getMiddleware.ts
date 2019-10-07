716e8c67249d453c4dfa0e1cb7612175c8a7e2e0
Herb Caudill <herb@devresults.com>
2019-10-07 10:29:44 +0200

    core/getMiddleware.ts: await await await

--- packages/core/src/getMiddleware.ts
+++ packages/core/src/getMiddleware.ts
@@ -7,7 +7,7 @@ import { ChangeSet, RepoSnapshot, MiddlewareFactory } from './types'
 const log = debug('cevitxe:middleware')
 
 export const getMiddleware: MiddlewareFactory = (repo, proxyReducer) => {
-  return store => next => action => {
+  return store => next => async action => {
     // BEFORE CHANGES
 
     // detect which documents will be changed and cache them
@@ -29,7 +29,7 @@ export const getMiddleware: MiddlewareFactory = (repo, proxyReducer) => {
         } else if (typeof fn === 'function') {
           // Doc will be run through a change function. Cache the previous version of the doc so we
           // can record changes for the storage feed
-          const oldDoc = repo.get(documentId) || A.init() // create a new doc if one doesn't exist
+          const oldDoc = (await repo.get(documentId)) || A.init() // create a new doc if one doesn't exist
           affectedDocs[documentId] = oldDoc
         }
       }
@@ -48,15 +48,15 @@ export const getMiddleware: MiddlewareFactory = (repo, proxyReducer) => {
     if (action.type === RECEIVE_MESSAGE_FROM_PEER) {
       // for changes coming from peer, we already have the Automerge changes, so just persist them
       const { documentId, changes } = action.payload.message
-      const newDoc = repo.get(documentId)
-      repo.saveSnapshot(documentId, newDoc)
+      const newDoc = await repo.get(documentId)
+      await repo.saveSnapshot(documentId, newDoc)
       changeSets.push({ documentId, changes })
     } else {
       // for insert/update, we generate the changes by comparing each document before & after
       for (const documentId in affectedDocs) {
         const oldDoc = affectedDocs[documentId]
-        const newDoc = repo.get(documentId)!
-        repo.saveSnapshot(documentId, newDoc)
+        const newDoc = await repo.get(documentId)!
+        await repo.saveSnapshot(documentId, newDoc)
         const changes = A.getChanges(oldDoc, newDoc)
         if (changes.length > 0) changeSets.push({ documentId, changes })
       }
@@ -73,7 +73,7 @@ export const getMiddleware: MiddlewareFactory = (repo, proxyReducer) => {
 
     // write any changes to the feed
     for (const changeSet of changeSets) {
-      repo.appendChangeset(changeSet)
+      await repo.appendChangeset(changeSet)
     }
 
     return newState
