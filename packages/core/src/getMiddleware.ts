d9af2381363033f9bc81259f3b30f19fad12a597
Herb Caudill <herb@devresults.com>
2019-10-07 14:03:25 +0200

    core/getMiddleware.ts: look at all that complexity, gone ✨

--- packages/core/src/getMiddleware.ts
+++ packages/core/src/getMiddleware.ts
@@ -1,41 +1,12 @@
-import A from 'automerge'
 import debug from 'debug'
-import { collection } from './collection'
-import { DELETE_COLLECTION, RECEIVE_MESSAGE_FROM_PEER } from './constants'
-import { ChangeSet, RepoSnapshot, MiddlewareFactory } from './types'
+import { RECEIVE_MESSAGE_FROM_PEER } from './constants'
+import { MiddlewareFactory } from './types'
 
 const log = debug('cevitxe:middleware')
 
 export const getMiddleware: MiddlewareFactory = (repo, proxyReducer) => {
   return store => next => async action => {
     // BEFORE CHANGES
-
-    // detect which documents will be changed and cache them
-    const affectedDocs: RepoSnapshot = {} // cache for docs that will be changed
-    const removedDocs: string[] = [] // list of docs that will be removed
-
-    const functionMap = proxyReducer(store.getState(), action)
-    if (functionMap) {
-      for (let documentId in functionMap) {
-        const fn = functionMap[documentId]
-        if (fn === DELETE_COLLECTION) {
-          log('DELETE_COLLECTION')
-          const name = collection.getCollectionName(documentId)
-          const documentIds = collection(name).selectors.keys(store.getState(), {
-            includeDeleted: true,
-          })
-          // Record each doc as removed so we can note that in the storage feed
-          for (const itemdocumentId of documentIds) removedDocs.push(itemdocumentId)
-        } else if (typeof fn === 'function') {
-          // Doc will be run through a change function. Cache the previous version of the doc so we
-          // can record changes for the storage feed
-          const oldDoc = (await repo.get(documentId)) || A.init() // create a new doc if one doesn't exist
-          await repo.change(documentId, fn)
-
-          affectedDocs[documentId] = oldDoc
-        }
-      }
-    }
 
     // CHANGES
     const newState = next(action)
@@ -44,38 +15,23 @@ export const getMiddleware: MiddlewareFactory = (repo, proxyReducer) => {
 
     log('%o', { action })
 
-    // collect document changes for persistence
-    const changeSets: ChangeSet[] = []
+    const functionMap = proxyReducer(store.getState(), action)
 
     if (action.type === RECEIVE_MESSAGE_FROM_PEER) {
-      // for changes coming from peer, we already have the Automerge changes, so just persist them
+      // pass any changes coming from the peer to the repo
       const { documentId, changes } = action.payload.message
-      const newDoc = await repo.get(documentId)
-      await repo.setSnapshot(documentId, newDoc)
-      changeSets.push({ documentId, changes })
-    } else {
-      // for insert/update, we generate the changes by comparing each document before & after
-      for (const documentId in affectedDocs) {
-        const oldDoc = affectedDocs[documentId]
-        const newDoc = await repo.get(documentId)!
-        await repo.setSnapshot(documentId, newDoc)
-        const changes = A.getChanges(oldDoc, newDoc)
-        if (changes.length > 0) changeSets.push({ documentId, changes })
-      }
-      // for remove actions, we've made a list, so we just add a flag for each
-      for (const documentId of removedDocs) {
-        const changeSet: ChangeSet = {
-          documentId,
-          changes: [],
-          isDelete: true,
+      log('apply message from peer', documentId)
+      const newDoc = await repo.applyChanges(documentId, changes)
+      newState[documentId] = { ...newDoc }
+    } else if (functionMap) {
+      for (let documentId in functionMap) {
+        // apply change functions via the repo
+        const fn = functionMap[documentId]
+        if (typeof fn === 'function') {
+          log('apply change function', documentId)
+          await repo.change(documentId, fn)
         }
-        changeSets.push(changeSet)
       }
-    }
-
-    // write any changes to the feed
-    for (const changeSet of changeSets) {
-      await repo.appendChangeset(changeSet)
     }
 
     return newState
