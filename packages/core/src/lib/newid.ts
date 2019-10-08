5479c866ae2676d0e3c6da697edd573c6ff61dac
Herb Caudill <herb@devresults.com>
2019-10-08 16:33:57 +0200

    core/lib/newid.ts: remove (use the one in signal-client)

--- packages/core/src/lib/newid.ts
+++
@@ -1,4 +0,0 @@
-﻿import uuid from 'uuid'
-// use shorter ids in development & testing
-export const newid = (len: number = 4) =>
-  process.env.NODE_ENV === 'production' ? uuid() : uuid().slice(0, len)
