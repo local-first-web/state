bf95b84773494e4361979358ce8ae8e6a23757f4
Herb Caudill <herb@devresults.com>
2019-10-08 16:34:44 +0200

    core/Message.ts: new message types wip

--- packages/core/src/Message.ts
+++ packages/core/src/Message.ts
@@ -2,21 +2,28 @@ import A from 'automerge'
 
 type Clock = Map<string, number>
 
-export const REQUEST_REPO = 'REQUEST_REPO'
+// export const REQUEST_INDEX = 'REQUEST_INDEX'
+export const SEND_INDEX = 'SEND_INDEX'
+
+// export const SEND_SNAPSHOTS = 'SEND_SNAPSHOTS'
+// export const SEND_HISTORIES = 'SEND_HISTORIES'
+
+
+
 export const REQUEST_DOC = 'REQUEST_DOC'
 export const ADVERTISE_DOC = 'ADVERTISE_DOC'
 export const REQUEST_CHANGES = 'REQUEST_CHANGES'
 export const SEND_CHANGES = 'SEND_CHANGES'
-export const SEND_ALL_CHANGES = 'SEND_ALL_CHANGES'
-export const SEND_SNAPSHOT = 'SEND_SNAPSHOT'
-export const SEND_ALL_SNAPSHOTS = 'SEND_ALL_SNAPSHOTS'
+// export const SEND_ALL_CHANGES = 'SEND_ALL_CHANGES'
+// export const SEND_SNAPSHOT = 'SEND_SNAPSHOT'
+// export const SEND_ALL_SNAPSHOTS = 'SEND_ALL_SNAPSHOTS'
 
-/**
- * Initializing repo from the network, get everything (snapshots and changes)
- */
-interface RequestRepoMessage {
-  type: typeof REQUEST_REPO
-}
+// /**
+//  * Initializing repo from the network, get everything (snapshots and changes)
+//  */
+// interface RequestIndexMessage {
+//   type: typeof REQUEST_INDEX
+// }
 
 /**
  * Initializing a new document from the netwrok, get everything (snapshot and changes)
@@ -55,44 +62,44 @@ interface SendChangesMessage {
   changes: A.Change[]
 }
 
-/**
- * Send all changes for all documents (for initialization)
- */
-interface SendAllMessage {
-  type: typeof SEND_ALL_CHANGES
-  changes: {
-    documentId: string
-    clock: Clock
-    changes: A.Change[]
-  }[]
-}
+// /**
+//  * Send all changes for all documents (for initialization)
+//  */
+// interface SendAllMessage {
+//   type: typeof SEND_ALL_CHANGES
+//   changes: {
+//     documentId: string
+//     clock: Clock
+//     changes: A.Change[]
+//   }[]
+// }
 
-/**
- * Send snapshot for a document
- */
-interface SendSnapshotMessage {
-  type: typeof SEND_SNAPSHOT
-  documentId: string
-  snapshot: any
-}
+// /**
+//  * Send snapshot for a document
+//  */
+// interface SendSnapshotMessage {
+//   type: typeof SEND_SNAPSHOT
+//   documentId: string
+//   snapshot: any
+// }
 
-/**
- * Send snapshots for all documents
- */
-interface SendAllSnapshotsMessage {
-  type: typeof SEND_ALL_SNAPSHOTS
-  snapshots: {
-    documentId: string
-    snapshot: any
-  }[]
-}
+// /**
+//  * Send snapshots for all documents
+//  */
+// interface SendAllSnapshotsMessage {
+//   type: typeof SEND_ALL_SNAPSHOTS
+//   snapshots: {
+//     documentId: string
+//     snapshot: any
+//   }[]
+// }
 
 export type Message =
-  | RequestRepoMessage
+  // | RequestRepoMessage
   | RequestDocMessage
   | AdvertiseDocMessage
   | RequestChangesMessage
   | SendChangesMessage
-  | SendAllMessage
-  | SendSnapshotMessage
-  | SendAllSnapshotsMessage
+  // | SendAllMessage
+  // | SendSnapshotMessage
+  // | SendAllSnapshotsMessage
