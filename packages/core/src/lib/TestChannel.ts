b18af6b2d65a6b3b73a2b68e35b1ca98b503397b
Herb Caudill <herb@devresults.com>
2019-10-08 16:34:18 +0200

    core/lib/TestChannel.ts: hold messages while waiting for peer

--- packages/core/src/lib/TestChannel.ts
+++ packages/core/src/lib/TestChannel.ts
@@ -1,6 +1,8 @@
 ﻿import { EventEmitter } from 'events'
 import { Message } from '../Message'
+import debug from 'debug'
 
+const log = debug('cevitxe:testchannel')
 /**
  * Simplest possible communications channel, for testing.
  *
@@ -14,7 +16,26 @@ import { Message } from '../Message'
  * ```
  */
 export class TestChannel extends EventEmitter {
+  addPeer() {
+    this.peers++
+    if (this.peers > 1) {
+      log('sending buffer')
+      for (const { id, msg } of this.buffer) {
+        log('emitting', id, msg)
+        this.emit('data', id, msg)
+      }
+      this.buffer = []
+    }
+  }
+  private peers: number = 0
+  private buffer: { id: string; msg: Message }[] = []
   write(id: string, msg: Message) {
-    this.emit('data', id, msg)
+    if (this.peers > 1) {
+      log('emitting', id, msg)
+      this.emit('data', id, msg)
+    } else {
+      log('buffering', id, msg)
+      this.buffer.push({ id, msg })
+    }
   }
 }
