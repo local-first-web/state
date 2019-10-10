cf42904b630bb480f8fa292ba2bbc4ae10b6b8e2
Herb Caudill <herb@devresults.com>
2019-10-10 16:23:19 +0200

    core/lib/lessOrEqual.ts: allow passing plain JS clocks

--- packages/core/src/lib/lessOrEqual.ts
+++ packages/core/src/lib/lessOrEqual.ts
@@ -2,11 +2,19 @@
 type Clock = Map<string, number>
 
 export const lessOrEqual = (clock1: Clock, clock2: Clock) => {
-  const clockIsLessOrEqual = (key: string) => clock1.get(key, 0) <= clock2.get(key, 0)
-  const clockSeq1 = clock1.keySeq().toJS() as string[]
-  const clockSeq2 = clock2.keySeq().toJS() as string[]
-  const both = clockSeq1.concat(clockSeq2)
-  return both.map(clockIsLessOrEqual).reduce(allTrue, true)
+  // coerce to plain JS
+  clock1 = clock1.toJS ? (clock1.toJS() as Clock) : clock1
+  clock2 = clock2.toJS ? (clock2.toJS() as Clock) : clock1
+
+  // get a list of all the keys
+  const actors1 = Object.keys(clock1)
+  const actors2 = Object.keys(clock2)
+
+  const allActors = actors1.concat(actors2) // not worth the effort of deduplicating
+
+  // @ts-ignore
+  const clockIsLessOrEqual = (key: string) => (clock1[key] || 0) <= (clock2[key] || 0)
+  return allActors.map(clockIsLessOrEqual).reduce(allTrue, true)
 }
 
 const allTrue = (acc: boolean, d: boolean): boolean => acc && d
