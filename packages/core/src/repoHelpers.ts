6c953f5697a881afb3a210a2c9e6f9b25e2625a2
Herb Caudill <herb@devresults.com>
2019-10-10 11:56:30 +0200

    core/repoHelpers.ts: remove unused

--- packages/core/src/repoHelpers.ts
+++ packages/core/src/repoHelpers.ts
@@ -5,18 +5,6 @@ import { newid } from 'cevitxe-signal-client'
 
 /**
  * DEPRECATED
- * Use Repo.getFullSnapshot
- */
-export const repoToObject = <T = any>(repo: Repo<T>): RepoSnapshot<T> => {
-  const result = {} as any
-  for (let documentId of repo.documentIds) {
-    result[documentId] = repo.get(documentId)
-  }
-  return result
-}
-
-/**
- * DEPRECATED
  * This is only used in tests
  */
 export const repoFromObject = async (obj: any): Promise<Repo<any>> => {
