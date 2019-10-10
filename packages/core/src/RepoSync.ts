c9633117b111b8081231a5cbbb54f821ce68a058
Herb Caudill <herb@devresults.com>
2019-10-10 19:36:14 +0200

    RepoSync.ts: 2 functions that should be Automerge's responsibility

--- packages/core/src/RepoSync.ts
+++ packages/core/src/RepoSync.ts
@@ -1,4 +1,4 @@
-﻿import A from 'automerge'
+import A from 'automerge'
 import debug from 'debug'
 import { Map } from 'immutable'
 import { Repo } from './Repo'
@@ -20,6 +20,22 @@ type ClockMap = Map<string, Clock>
 
 const EMPTY_CLOCK: Clock = Map()
 const EMPTY_CLOCKMAP: ClockMap = Map()
+
+// TODO: Submit these to Automerge
+const _A = {
+  ...A,
+
+  getMissingChanges: (ourDoc: A.Doc<any>, theirClock: A.Clock): A.Change[] => {
+    if (theirClock === undefined) return []
+    const ourState = A.Frontend.getBackendState(ourDoc)
+    return A.Backend.getMissingChanges(ourState!, theirClock)
+  },
+
+  getClock: (doc: A.Doc<any>): Clock => {
+    const state = A.Frontend.getBackendState(doc) as any // BackendState doesn't have a public API
+    return state.getIn(['opSet', 'clock']) as Clock
+  },
+}
 
 /**
  * One instance of `RepoSync` keeps one local document in sync with one remote peer's replica of the
