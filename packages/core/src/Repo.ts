69592d60a4d05244565d09d70541da9299d376bd
Herb Caudill <herb@devresults.com>
2019-10-11 12:22:37 +0200

    Repo.ts: add TODO to ensure open() is called

--- packages/core/src/Repo.ts
+++ packages/core/src/Repo.ts
@@ -49,11 +49,15 @@ export class Repo<T = any> extends EventEmitter {
    */
   public databaseName: string
 
+  /**
+   * Unique identifier representing this peer
+   */
   public clientId: string
+
   /**
    * In-memory map of document snapshots.
    */
-  private state: RepoSnapshot<T>
+  private state: RepoSnapshot<T> = {}
 
   private docCache: Cache<string, any>
 
@@ -69,7 +73,6 @@ export class Repo<T = any> extends EventEmitter {
     this.discoveryKey = discoveryKey
     this.databaseName = databaseName
     this.log = debug(`cevitxe:repo:${databaseName}`)
-    this.state = {}
     this.handlers = new Set()
     this.docCache = new Cache({ max: 1000 })
     this.clientId = clientId
@@ -82,6 +85,11 @@ export class Repo<T = any> extends EventEmitter {
    * @returns An `idb` wrapper for an indexed DB.
    */
   async open() {
+    // TODO: add helper method to ensure open() is called
+    // see https://github.com/DevResults/cevitxe/pull/24#discussion_r333490817 : Since this is 100%
+    // required (it's the async part of construction), I suggest we add a flag (set at the end of
+    // open) and a helper method that checks it and throws if the repo isn't open yet. All the
+    // instance methods would call it to avoid hard-to-track bugs.
     const storageKey = `cevitxe::${this.databaseName}::${this.discoveryKey.substr(0, 12)}`
     this.database = await idb.openDB(storageKey, DB_VERSION, {
       upgrade(db) {
@@ -128,7 +136,7 @@ export class Repo<T = any> extends EventEmitter {
       await this.createFromSnapshot({})
     } else {
       this.log('recovering an existing repo from persisted state')
-      await this.rebuildSnapshotsFromHistory()
+      await this.loadSnapshotsFromDb()
     }
     this.emit('ready')
     return this.state
@@ -165,20 +173,29 @@ export class Repo<T = any> extends EventEmitter {
   }
 
   /**
+   * @returns true if this repo has this document (even if it's been deleted)
+   */
+  has(documentId: string): boolean {
+    // if the document has been deleted, its snapshot set to `null`, but the map still contains the entry
+    return this.state.hasOwnProperty(documentId)
+  }
+
+  /**
+   * Returns the number of document IDs that this repo has (including deleted)
+   */
+  get count() {
+    return this.documentIds.length
+  }
+
+  /**
    * Reconstitutes an Automerge document from its change history
    * @param documentId
    */
   async get(documentId: string): Promise<A.Doc<T>> {
+    // TODO: reimplement caching
     this.log('get', documentId)
-    // if (!this.docCache.has(documentId)) {
     const doc = await this.reconstructDoc(documentId)
     return doc
-    // await this.saveSnapshot(documentId, doc)
-    // this.docCache.set(documentId, doc)
-    // } else {
-    // }
-    // const cachedDoc = this.docCache.get(documentId)
-    // return cachedDoc
   }
 
   /**
@@ -304,7 +321,8 @@ export class Repo<T = any> extends EventEmitter {
   }
 
   /**
-   * Removes the snapshot with the given `documentId` from in-memory state.
+   * Removes the snapshot with the given `documentId` from in-memory state. (More precisely, sets it
+   * to `null` as a marker that we've seen the document before.)
    * @param documentId
    */
   removeSnapshot(documentId: string) {
@@ -349,10 +367,9 @@ export class Repo<T = any> extends EventEmitter {
    * Used when receiving the entire current state of a repo from a peer.
    */
   async loadHistory(history: RepoHistory) {
-    // TODO: clear current changes?
     for (const documentId in history) {
       const changes = history[documentId]
-      await this.appendChangeset({ documentId, changes })
+      await this.applyChanges(documentId, changes)
     }
   }
 
@@ -376,14 +393,10 @@ export class Repo<T = any> extends EventEmitter {
   /**
    * Loads all the repo's snapshots into memory
    */
-  private async rebuildSnapshotsFromHistory() {
+  private async loadSnapshotsFromDb() {
     const snapshots = await this.database!.getAll('snapshots')
     for (const { documentId, snapshot } of snapshots) {
-      if (snapshot === null || snapshot[DELETED]) {
-        // omit deleted documents
-      } else {
-        this.state[documentId] = snapshot
-      }
+      this.state[documentId] = snapshot[DELETED] ? null : snapshot
     }
   }
 
