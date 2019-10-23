3ee85dc84a34de28dada72d759b8dec1bd27a35c
Herb Caudill <herb@devresults.com>
2019-10-23 18:30:15 +0200

    clocks.ts: simplify isMoreRecent logic

--- packages/core/src/clocks.ts
+++ packages/core/src/clocks.ts
@@ -18,6 +18,7 @@ export function mergeClocks(_oldClock: PlainClock, _clock: PlainClock): PlainClo
   return R.mergeWith(largestWins, _oldClock, _clock)
 }
 
+// temporary adapter
 export const isMoreRecent_old = (clock1: Clock, clock2: Clock) => {
   // coerce to plain JS
   const _clock1 = clock1.toJS() as PlainClock
@@ -27,15 +28,7 @@ export const isMoreRecent_old = (clock1: Clock, clock2: Clock) => {
 }
 
 export const isMoreRecent = (clock1: PlainClock, clock2: PlainClock) => {
-  // get a list of all the keys
-  const actors1 = Object.keys(clock1)
-  const actors2 = Object.keys(clock2)
-
-  // there will be duplicates, but it's not worth the effort of deduplicating
-  const allActors = actors1.concat(actors2)
-
-  const clockIsLessOrEqual = (key: string) => (clock1[key] || 0) <= (clock2[key] || 0)
-  return !allActors.map(clockIsLessOrEqual).reduce(allTrue, true)
+  const actors = Object.keys({ ...clock1, ...clock2 })
+  const clockIsMoreRecent = (actorId: string) => (clock1[actorId] || 0) > (clock2[actorId] || 0)
+  return actors.some(clockIsMoreRecent)
 }
-
-const allTrue = (acc: boolean, d: boolean): boolean => acc && d
