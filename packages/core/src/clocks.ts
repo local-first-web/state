84e5b1cefa8ea631062b506559bc0a524ee48055
Herb Caudill <herb@devresults.com>
2019-10-23 19:49:04 +0200

    clocks.ts:

--- packages/core/src/clocks.ts
+++ packages/core/src/clocks.ts
@@ -1,11 +1,12 @@
 ﻿import * as R from 'ramda'
 import { Clock } from './types'
 
-export function mergeClocks(_oldClock: Clock, _clock: Clock): Clock {
+/** Merges the clocks, keeping the maximum sequence number for each node */
+export function mergeClocks(oldClock: Clock, newClock: Clock): Clock {
   const largestWins = (x: number = 0, y: number = 0): number => Math.max(x, y)
-  return R.mergeWith(largestWins, _oldClock, _clock)
+  return R.mergeWith(largestWins, oldClock, newClock)
 }
-
+/** Returns true if there are any actors in clock1 that have more recent updates than in clock2 */
 export const isMoreRecent = (clock1: Clock, clock2: Clock) => {
   const actors = Object.keys({ ...clock1, ...clock2 })
   const clockIsMoreRecent = (actorId: string) => (clock1[actorId] || 0) > (clock2[actorId] || 0)
