389cafe1badc687bef7a9e2e8a2ff09b30e600fb
Herb Caudill <herb@devresults.com>
2019-10-25 11:45:37 +0200

    clocks.ts: handle conversion to/from A.Clock

--- packages/core/src/clocks.ts
+++ packages/core/src/clocks.ts
@@ -1,7 +1,7 @@
 ﻿import * as R from 'ramda'
 import { Clock } from './types'
 import A from 'automerge'
-
+import { Map } from 'immutable'
 /**
  * A vector clock is a map, where the keys are the actorIds of all actors that have been active on a
  * particular document, and the values are the most recent sequence number for that actor. The
@@ -24,14 +24,22 @@ export const isMoreRecent = (clock1: Clock, clock2: Clock) => {
 }
 
 // TODO: These probably should be top-level Automerge functions; submit PR
+const _A = {
+  ...A,
+  getMissingChanges: (ourDoc: A.Doc<any>, theirClock: A.Clock): A.Change[] => {
+    if (theirClock === undefined) return []
+    const ourState = A.Frontend.getBackendState(ourDoc)
+    return A.Backend.getMissingChanges(ourState!, theirClock)
+  },
 
-export const getMissingChanges = (ourDoc: A.Doc<any>, theirClock: A.Clock): A.Change[] => {
-  if (theirClock === undefined) return []
-  const ourState = A.Frontend.getBackendState(ourDoc)
-  return A.Backend.getMissingChanges(ourState!, theirClock)
+  getClock: (doc: A.Doc<any>): A.Clock => {
+    const state = A.Frontend.getBackendState(doc) as any // BackendState doesn't have a public API
+    return state.getIn(['opSet', 'clock']) as A.Clock
+  },
 }
 
-export const getClock = (doc: A.Doc<any>): A.Clock => {
-  const state = A.Frontend.getBackendState(doc) as any // BackendState doesn't have a public API
-  return state.getIn(['opSet', 'clock']) as A.Clock
-}
+// coerce our clocks to A.Clocks
+export const getMissingChanges = (ourDoc: A.Doc<any>, theirClock: Clock): A.Change[] =>
+  _A.getMissingChanges(ourDoc, (Map(theirClock) as unknown) as A.Clock)
+
+export const getClock = (doc: A.Doc<any>): Clock => Map(_A.getClock(doc)).toJS() as Clock
