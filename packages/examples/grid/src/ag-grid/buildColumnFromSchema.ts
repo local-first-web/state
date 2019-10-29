d314d7df76c118460df716ff588d46567eaf5a75
Herb Caudill <herb@devresults.com>
2019-10-29 13:28:18 +0100

    examples/grid/ag-grid/buildColumnFromSchema.ts: comments

--- packages/examples/grid/src/ag-grid/buildColumnFromSchema.ts
+++ packages/examples/grid/src/ag-grid/buildColumnFromSchema.ts
@@ -2,13 +2,19 @@
 
 export const buildColumnFromSchema = (field: string, schema: any) => {
   const colDef: ColDef = { field }
-  if (schema.description) {
-    colDef.headerName = schema.description
-  }
+
+  // Use description if present
+  if (schema.description) colDef.headerName = schema.description
+
+  // Default sort = displayOrder
+  if (field === 'displayOrder') colDef.sort = 'asc'
+
+  // Our only non-text column type for now
   switch (schema.type) {
     case 'number':
       colDef.type = 'numericColumn'
       colDef.filter = 'number'
   }
+
   return colDef
 }
