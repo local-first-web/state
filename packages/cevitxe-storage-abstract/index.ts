7532b9cbd18eb3fabfa784c0d73f9b8d9467b30c
Herb Caudill <herb@devresults.com>
2019-10-31 13:48:11 +0100

    cevitxe-storage-abstract/index.ts: rearrange methods

--- packages/cevitxe-storage-abstract/index.ts
+++ packages/cevitxe-storage-abstract/index.ts
@@ -10,15 +10,13 @@ export abstract class StorageAdapter {
 
   abstract async open(): Promise<void>
   abstract async close(): Promise<void>
-
-  abstract get snapshots(): AsyncIterableIterator<IteratorResult<SnapshotRecord, SnapshotRecord>>
-  abstract get changes(): AsyncIterableIterator<IteratorResult<ChangeSet, ChangeSet>>
-
   abstract async hasData(): Promise<boolean>
 
+  abstract get changes(): AsyncIterableIterator<IteratorResult<ChangeSet, ChangeSet>>
   abstract async getChanges(documentId: string): Promise<ChangeSet[]>
   abstract async appendChanges(changeSet: ChangeSet): Promise<void>
 
+  abstract get snapshots(): AsyncIterableIterator<IteratorResult<SnapshotRecord, SnapshotRecord>>
   abstract async putSnapshot(snapshotRecord: SnapshotRecord): Promise<void>
   abstract async deleteSnapshot(snapshotId: string): Promise<void>
 }
