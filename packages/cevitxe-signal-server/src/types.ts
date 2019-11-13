6f4381fb19991903fb2cb87619bc235333baf1aa
Herb Caudill <herb@devresults.com>
2019-11-13 12:29:24 +0100

    types.ts

--- packages/cevitxe-signal-server/src/types.ts
+++ packages/cevitxe-signal-server/src/types.ts
@@ -14,7 +14,7 @@ export namespace Message {
 
   export interface Join {
     type: 'Join' | 'Leave'
-    join?: string[]
+    join?: string[] // document IDs
     leave?: string[]
   }
 
@@ -22,7 +22,7 @@ export namespace Message {
 
   export interface Introduction {
     type: 'Introduction'
-    id: string
-    keys: string[]
+    id: string // the other peer we're introducing this client to
+    keys: string[] // document IDs
   }
 }
