4c089d725e017eaf6a0f7c269f57ff5e8c772189
Herb Caudill <herb@devresults.com>
2019-12-01 15:00:17 +0100

    cevitxe/StoreManager.ts: RepoSnapshot no longer generic

--- packages/cevitxe/src/StoreManager.ts
+++ packages/cevitxe/src/StoreManager.ts
@@ -21,7 +21,7 @@ let log = debug('cevitxe:StoreManager')
 export class StoreManager<T> {
   private databaseName: string
   private proxyReducer: ProxyReducer
-  private initialState: RepoSnapshot<T>
+  private initialState: RepoSnapshot
   private urls: string[]
   private middlewares: Middleware[] // TODO: accept an `enhancer` object instead
   private repo?: Repo
@@ -70,7 +70,7 @@ export class StoreManager<T> {
     return this.store
   }
 
-  private createReduxStore(initialState: RepoSnapshot<T>) {
+  private createReduxStore(initialState: RepoSnapshot) {
     if (!this.repo) throw new Error(`Can't create Redux store without repo`)
     const reducer = getReducer(this.proxyReducer, this.repo)
     const cevitxeMiddleware = getMiddleware(this.repo, this.proxyReducer)
@@ -110,7 +110,7 @@ export interface StoreManagerOptions<T> {
   /** Redux middlewares to add to the store. */
   middlewares?: Middleware[]
   /** The starting state of a blank document. */
-  initialState: RepoSnapshot<T>
+  initialState: RepoSnapshot
   /** A name for the storage feed, to distinguish this application's data from any other Cevitxe data stored on the same machine. */
   databaseName: string
   /** The address(es) of one or more signal servers to try. */
