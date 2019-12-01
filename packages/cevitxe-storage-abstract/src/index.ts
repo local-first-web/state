2b06e048fe4ca1c3f261fdcefba3ad5d2f031e47
Herb Caudill <herb@devresults.com>
2019-12-01 15:20:12 +0100

    cevitxe-storage-abstract/index.ts: add collection params to changes(), snapshots()

--- packages/cevitxe-storage-abstract/src/index.ts
+++ packages/cevitxe-storage-abstract/src/index.ts
@@ -2,21 +2,23 @@
 
 export abstract class StorageAdapter {
   storageKey: string
+  collections: string[]
 
-  constructor(options: { discoveryKey: string; databaseName: string }) {
-    const { databaseName, discoveryKey } = options
+  constructor(options: { discoveryKey: string; databaseName: string; collections?: string[] }) {
+    const { databaseName, discoveryKey, collections = [] } = options
     this.storageKey = `cevitxe_${databaseName}_${discoveryKey.substr(0, 12)}`
+    this.collections = collections
   }
 
   abstract async open(): Promise<void>
   abstract async close(): Promise<void>
   abstract async hasData(): Promise<boolean>
 
-  abstract changes(): AsyncIterableIterator<ChangeSet>
+  abstract changes(collection?: string): AsyncIterableIterator<ChangeSet>
   abstract async getChanges(documentId: string): Promise<ChangeSet[]>
   abstract async appendChanges(changeSet: ChangeSet): Promise<void>
 
-  abstract snapshots(): AsyncIterableIterator<SnapshotRecord>
+  abstract snapshots(collection?: string): AsyncIterableIterator<SnapshotRecord>
   abstract async putSnapshot(snapshotRecord: SnapshotRecord): Promise<void>
   abstract async deleteSnapshot(snapshotId: string): Promise<void>
 }
