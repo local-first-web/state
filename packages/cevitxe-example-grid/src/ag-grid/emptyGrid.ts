e1fe3bc69e8899c62a9121a35a3044a1e48e1a8d
Herb Caudill <herb@devresults.com>
2020-04-05 15:28:16 +0200

    emptyGrid.ts: don't need to know collection internals now

--- packages/cevitxe-example-grid/src/ag-grid/emptyGrid.ts
+++ packages/cevitxe-example-grid/src/ag-grid/emptyGrid.ts
@@ -1,16 +1,9 @@
 import { JSONSchema7 } from 'json-schema'
 import { range } from 'ramda'
 
-const rowCollectionKey = '__rows'
-
 export const emptyGrid = (rowCount: number, colCount: number = rowCount) => {
   const rows = range(0, rowCount).map(i => `row_${i + 1}`)
   const cols = range(0, colCount).map(i => `col_${i + 1}`)
-
-  const rowReducer = (rowMap: { [id: string]: any }, id: string) => ({
-    ...rowMap,
-    [`${rowCollectionKey}__${id}`]: { id },
-  })
 
   const columnReducer = (colMap: JSONSchema7['properties'], id: string, i: number) => ({
     ...colMap,
@@ -18,7 +11,7 @@ export const emptyGrid = (rowCount: number, colCount: number = rowCount) => {
   })
 
   return {
-    ...rows.reduce(rowReducer, {}),
+    ...rows,
     schema: { properties: cols.reduce(columnReducer, {}) },
   }
 }
