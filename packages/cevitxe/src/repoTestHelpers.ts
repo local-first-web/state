0fc0fb6a2495173bc2d8a9da0da59899e654faeb
Herb Caudill <herb@devresults.com>
2020-04-05 11:42:34 +0200

    cevitxe/repoTestHelpers.ts: add collection list

--- packages/cevitxe/src/repoTestHelpers.ts
+++ packages/cevitxe/src/repoTestHelpers.ts
@@ -1,8 +1,15 @@
 import { newid } from 'cevitxe-signal-client'
 import { Repo } from './Repo'
 
-export const repoFromSnapshot = async (obj: any): Promise<Repo<any>> => {
-  const repo = new Repo<any>({ discoveryKey: 'brilliant-test', databaseName: `testdb-${newid()}` })
+export const repoFromSnapshot = async (
+  obj: any,
+  collections: string[] = []
+): Promise<Repo<any>> => {
+  const repo = new Repo<any>({
+    discoveryKey: 'brilliant-test',
+    databaseName: `testdb-${newid()}`,
+    collections,
+  })
   await repo.open()
   await repo.createFromSnapshot(obj)
   return repo
