df5f7009ae9ddac5c88c25636581cf36aec07e6c
Herb Caudill <herb@devresults.com>
2020-04-07 09:51:21 +0200

    cevitxe-example-grid/redux/store.ts: use <GridState>

--- packages/cevitxe-example-grid/src/redux/store.ts
+++ packages/cevitxe-example-grid/src/redux/store.ts
@@ -1,14 +1,15 @@
 import { StoreManager } from 'cevitxe'
 import { emptyGrid } from '../ag-grid/emptyGrid'
 import { proxyReducer } from './reducers'
+import { GridState } from 'types'
 
-const initialState = emptyGrid(3, 3)
+const initialState = emptyGrid(3, 3) as GridState
 
 const urls = process.env.REACT_APP_SIGNAL_SERVERS
   ? process.env.REACT_APP_SIGNAL_SERVERS.split(',')
   : undefined
 
-export const storeManager = new StoreManager({
+export const storeManager = new StoreManager<GridState>({
   databaseName: 'grid',
   proxyReducer,
   collections: ['rows'],
