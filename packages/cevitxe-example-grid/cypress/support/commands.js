d843e72383bb44643a9ca8b97e1fac65b65899f6
Herb Caudill <herb@devresults.com>
2020-04-07 09:49:48 +0200

    cevitxe-example-grid/cypress/support/commands.js:

--- packages/cevitxe-example-grid/cypress/support/commands.js
+++ packages/cevitxe-example-grid/cypress/support/commands.js
@@ -4,12 +4,40 @@ import { codes } from 'keycode'
 const keycode = name => ({ keyCode: codes[name], which: codes[name] })
 
 Cypress.Commands.add('firstCell', () => cy.get('.ag-cell:first').click())
+
 Cypress.Commands.add('focusedCell', () => cy.get('.ag-cell-focus'))
-Cypress.Commands.add('cell', (x, y) => cy.get(`.ag-row[row-id=${y}] .ag-cell[col-id=${x}]`).click())
-Cypress.Commands.add('columnMenu', (x) => cy.columns().eq(x).find('.ag-header-cell-menu-button').click())
+
+Cypress.Commands.add('cell', (x, y) =>
+  cy
+    .rows()
+    .eq(y)
+    .find('.ag-cell')
+    .eq(x)
+    .click()
+)
+
+Cypress.Commands.add('columnMenu', x =>
+  cy
+    .columns()
+    .eq(x)
+    .find('.ag-header-cell-menu-button')
+    .click()
+)
+
+Cypress.Commands.add('columnTypeMenu', x =>
+  cy
+    .columns()
+    .eq(x)
+    .find('.ag-header-cell-menu-button')
+    .click()
+    .queryByText('Change column type')
+    .click()
+)
 
 Cypress.Commands.add('columns', () => cy.get('.ag-header-cell'))
+
 Cypress.Commands.add('rows', () => cy.get('.ag-center-cols-viewport .ag-row'))
+
 Cypress.Commands.add('selectedCells', () => cy.get('.ag-cell-range-selected'))
 
 Cypress.Commands.add(
@@ -39,11 +67,17 @@ Cypress.Commands.add('typeInCell', text => {
 Cypress.Commands.add('enter', text => cy.focusedCell().type('{enter}'))
 
 Cypress.Commands.add('up', () => cy.focusedCell().type('{uparrow}'))
+
 Cypress.Commands.add('right', () => cy.focusedCell().type('{rightarrow}'))
+
 Cypress.Commands.add('down', () => cy.focusedCell().type('{downarrow}'))
+
 Cypress.Commands.add('left', () => cy.focusedCell().type('{leftarrow}'))
 
 Cypress.Commands.add('shiftUp', () => cy.focusedCell().type('{shift}{uparrow}'))
+
 Cypress.Commands.add('shiftRight', () => cy.focusedCell().type('{shift}{rightarrow}'))
+
 Cypress.Commands.add('shiftDown', () => cy.focusedCell().type('{shift}{downarrow}'))
+
 Cypress.Commands.add('shiftLeft', () => cy.focusedCell().type('{shift}{leftarrow}'))
