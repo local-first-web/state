4f8c0c3c129abcb87f637ad49b1391e5c1bff577
Herb Caudill <herb@devresults.com>
2020-04-07 09:48:52 +0200

    cevitxe/getMiddleware.ts:

--- packages/cevitxe/src/getMiddleware.ts
+++ packages/cevitxe/src/getMiddleware.ts
@@ -1,9 +1,10 @@
 import debug from 'debug'
 import { Middleware } from 'redux'
-import { ProxyReducer } from 'cevitxe-types'
+import { ProxyReducer, ChangeMap } from 'cevitxe-types'
 import { DELETE_COLLECTION, RECEIVE_MESSAGE_FROM_PEER, GLOBAL } from './constants'
 import { Repo } from './Repo'
-import A from 'automerge'
+import A, { ChangeFn } from 'automerge'
+import { toArray } from './toArray'
 
 const log = debug('cevitxe:middleware')
 
@@ -16,34 +17,47 @@ export type MiddlewareFactory = (
 export const getMiddleware: MiddlewareFactory = (repo, proxyReducer) => {
   return store => next => async action => {
     // BEFORE CHANGES
-    // ...
+    // ... nothing to do here
 
-    // CHANGES
+    // MAKE CHANGES
     const newState = next(action)
 
     // AFTER CHANGES
 
     log('%o', { action })
 
-    const functionMap = proxyReducer(store.getState(), action)
-
     if (action.type === RECEIVE_MESSAGE_FROM_PEER) {
       // Synchronizer has already updated our repo - nothing to do here
     } else {
-      // Apply changes to Repo history
-      if (typeof functionMap === 'function') {
-        // Single function - apply to global object
-        const fn = functionMap as A.ChangeFn<any>
-        await repo.change(GLOBAL, fn)
+      // A reducer can return a function, a map of functions, or an array combining the two
+      const reducerOutput = proxyReducer(store.getState(), action)
+
+      // The reducer has already applied changes synchronously to repo snapshots. Here we persist
+      // the Automerge changes, which will also trigger synchronization with any peers we're
+      // connected to.
+
+      if (reducerOutput === null) {
+        // Nothing for us to do (could be an action handled elsewhere)
       } else {
-        // Multiple functions - apply to each document
-        for (let documentId in functionMap) {
-          const fn = functionMap[documentId]
-          if (fn === DELETE_COLLECTION) {
-            await repo.markCollectionAsDeleted(documentId)
-          } else if (typeof fn === 'function') {
-            // Apply change to each document
-            await repo.change(documentId, fn)
+        // Apply changes to Repo history
+        for (const fnMapOrFn of toArray(reducerOutput)) {
+          if (typeof fnMapOrFn === 'function') {
+            // Single function - apply to global object
+            const fn = fnMapOrFn as A.ChangeFn<any>
+            await repo.change(GLOBAL, fn)
+          } else {
+            // Multiple functions - apply to each document
+            for (let documentId in fnMapOrFn) {
+              const fnOrSymbol = fnMapOrFn[documentId]
+              if (fnOrSymbol === DELETE_COLLECTION) {
+                // Implement collection deletion flag
+                await repo.markCollectionAsDeleted(documentId)
+              } else if (typeof fnOrSymbol === 'function') {
+                // Apply change to each document
+                const fn = fnOrSymbol as ChangeFn<any>
+                await repo.change(documentId, fn)
+              }
+            }
           }
         }
       }
