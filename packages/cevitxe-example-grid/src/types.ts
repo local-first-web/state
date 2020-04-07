5a387649a94ea271a338fe39e1bbdee056230208
Herb Caudill <herb@devresults.com>
2020-04-07 09:50:34 +0200

    cevitxe-example-grid/types.ts: add GridState type

--- packages/cevitxe-example-grid/src/types.ts
+++ packages/cevitxe-example-grid/src/types.ts
@@ -1,3 +1,11 @@
 import { CSSObject } from '@emotion/core'
+import { JSONSchema7 } from 'json-schema'
 
 export type Stylesheet = { [k: string]: CSSObject }
+
+export type GridState = {
+  rows: {
+    [x: string]: any
+  }
+  schema: JSONSchema7
+}
