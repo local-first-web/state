23d6d9f196649a04d071ed75e1f1b516f5562e25
Herb Caudill <herb@devresults.com>
2020-04-10 11:50:07 +0200

    return ChangeManifests

--- packages/cevitxe-example-grid/src/redux/reducers.ts
+++ packages/cevitxe-example-grid/src/redux/reducers.ts
@@ -1,34 +1,60 @@
-import { collection } from 'cevitxe'
-import { ChangeFn, ChangeMap, ProxyReducer } from 'cevitxe-types'
+import { ChangeFn, ProxyReducer, ChangeManifest } from 'cevitxe-types'
 import { inferSchema } from 'inferSchema'
 import { JSONSchema7 } from 'json-schema'
-import * as actions from './actions'
 import { GridState } from 'types'
+import * as actions from './actions'
 
-const rows = collection('rows')
+const toArray = <T>(x: T | T[] | null) => (x === null ? [] : Array.isArray(x) ? x : [x])
+
 export const proxyReducer: ProxyReducer<GridState> = (state, { type, payload }) => {
-  const { add, update, change, remove, drop } = rows.reducers
   switch (type) {
-    case actions.ITEM_ADD:
-      return add(payload)
+    case actions.ITEM_ADD: {
+      const newRows = toArray(payload)
+      return newRows.map(newRow => ({
+        collection: 'rows',
+        id: newRow.id,
+        fn: row => Object.assign(row, newRow),
+      }))
+    }
 
-    case actions.ITEM_UPDATE:
-      return update(payload)
+    case actions.ITEM_UPDATE: {
+      const updatedRow = payload
+      return {
+        collection: 'rows',
+        id: payload.id,
+        fn: row => Object.assign(row, updatedRow),
+      }
+    }
 
     case actions.ITEM_REMOVE:
-      return remove(payload)
+      return {
+        collection: 'rows',
+        id: payload.id,
+        delete: true,
+      }
 
     case actions.COLLECTION_CLEAR:
-      return drop()
+      return {
+        collection: 'rows',
+        drop: true,
+      }
 
     case actions.COLLECTION_LOAD:
-      return add(payload.collection)
+      return toArray(payload.collection).map(newRow => ({
+        collection: 'rows',
+        id: newRow.id,
+        fn: row => Object.assign(row, newRow),
+      }))
 
     case actions.SCHEMA_LOAD:
-      return s => (s.schema = payload.schema)
+      return (s: GridState) => {
+        s.schema = payload.schema
+      }
 
     case actions.SCHEMA_INFER:
-      return s => (s.schema = inferSchema(payload.sampleData))
+      return s => {
+        s.schema = inferSchema(payload.sampleData)
+      }
 
     case actions.FIELD_ADD:
       return s => {
@@ -46,15 +72,21 @@ export const proxyReducer: ProxyReducer<GridState> = (state, { type, payload }) 
       const { id: fieldId } = payload
 
       // remove field from schema
-      const schemaChanges: ChangeFn<GridState> = s => {
+      const schemaChanges = (s: GridState) => {
         delete s.schema.properties![fieldId]
       }
 
       // delete each value from each row
-      const documentIds = Object.keys(state.rows)
-      const deleteField = (documentId: string) => change(documentId, d => delete d[fieldId])
+      const ids = Object.keys(state.rows)
+      const deleteField = (id: string) => ({
+        collection: 'rows',
+        id,
+        fn: (row: any) => {
+          delete row[fieldId]
+        },
+      })
 
-      return [schemaChanges, ...documentIds.map(deleteField)]
+      return [schemaChanges, ...ids.map(deleteField)]
     }
 
     case actions.FIELD_SET_TYPE: {
@@ -67,24 +99,27 @@ export const proxyReducer: ProxyReducer<GridState> = (state, { type, payload }) 
       }
 
       // update the column value in each row
-      const documentIds = Object.keys(state.rows)
-      const updateFieldType = (documentId: string) =>
-        change(documentId, d => {
-          if (d[fieldId] !== null) {
+      const ids = Object.keys(state.rows)
+      const updateFieldType = (id: string) => ({
+        collection: 'rows',
+        id,
+        fn: (row: any) => {
+          if (row[fieldId] !== null) {
             switch (newType) {
-              case 'number':
-                const number = Number(d[fieldId])
-                if (Number.isNaN(number)) d[fieldId] = ''
-                else d[fieldId] = number
-                break
-              case 'string':
-                d[fieldId] = String(d[fieldId])
-                break
+              case 'number': {
+                const number = Number(row[fieldId])
+                if (Number.isNaN(number)) row[fieldId] = ''
+                else row[fieldId] = number
+              }
+              case 'string': {
+                row[fieldId] = String(row[fieldId])
+              }
             }
           }
-        })
+        },
+      })
 
-      return [schemaChanges, ...documentIds.map(updateFieldType)]
+      return [schemaChanges, ...ids.map(updateFieldType)]
     }
 
     default:
