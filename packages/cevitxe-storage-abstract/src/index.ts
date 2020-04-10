3a976cb5687c0de47f1e36b58338f2285405db13
Herb Caudill <herb@devresults.com>
2020-04-10 15:04:33 +0200

    cevitxe-storage-abstract/index.ts: revert collection-aware stuff

--- packages/cevitxe-storage-abstract/src/index.ts
+++ packages/cevitxe-storage-abstract/src/index.ts
@@ -2,23 +2,21 @@
 
 export abstract class StorageAdapter {
   storageKey: string
-  collections: string[]
 
-  constructor(options: { discoveryKey: string; databaseName: string; collections?: string[] }) {
-    const { databaseName, discoveryKey, collections = [] } = options
+  constructor(options: { discoveryKey: string; databaseName: string }) {
+    const { databaseName, discoveryKey } = options
     this.storageKey = `cevitxe_${databaseName}_${discoveryKey.substr(0, 12)}`
-    this.collections = collections
   }
 
   abstract async open(): Promise<void>
   abstract async close(): Promise<void>
   abstract async hasData(): Promise<boolean>
 
-  abstract changes(collection?: string): AsyncIterableIterator<ChangeSet>
+  abstract changes(): AsyncIterableIterator<ChangeSet>
   abstract async getChanges(documentId: string): Promise<ChangeSet[]>
   abstract async appendChanges(changeSet: ChangeSet): Promise<void>
 
-  abstract snapshots(collection?: string): AsyncIterableIterator<SnapshotRecord>
+  abstract snapshots(): AsyncIterableIterator<SnapshotRecord>
   abstract async putSnapshot(snapshotRecord: SnapshotRecord): Promise<void>
   abstract async deleteSnapshot(snapshotId: string): Promise<void>
 }
