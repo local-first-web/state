a0f52692f187f2c4e96811c173f9af255db10e19
Herb Caudill <herb@devresults.com>
2020-04-10 11:45:43 +0200

    cevitxe/constants.ts: remove unused symbols

--- packages/cevitxe/src/constants.ts
+++ packages/cevitxe/src/constants.ts
@@ -4,10 +4,6 @@ export const INITIALIZE = 'cevitxe/INITIALIZE'
 
 export const DEFAULT_SIGNAL_SERVERS = ['ws://localhost:8080'] // default public signaling server
 
-// deletion symbols
-export const DELETE_COLLECTION = Symbol('DELETE_COLLECTION')
-export const DELETE_ITEM = Symbol('DELETE_ITEM')
-
 // deleted flag
 export const DELETED = '__DELETED'
 
