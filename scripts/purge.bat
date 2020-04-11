049a334b3fb353ed0fb80208de35b29eb3918a6c
Herb Caudill <herb@devresults.com>
2020-04-11 12:36:21 +0200

    straighten out cypress/jest/mocha type conflicts

--- scripts/purge.bat
+++ scripts/purge.bat
@@ -2,10 +2,3 @@ del yarn.lock
 
 FOR /d /r . %%d IN (node_modules) DO @IF EXIST "%%d" rd /s /q "%%d"
 FOR /d /r . %%d IN (dist) DO @IF EXIST "%%d" rd /s /q "%%d"
-
-
-FOR /d /r . %%d IN (node_modules) DO @IF EXIST "%%d" rd /s /q "%%d"
-FOR /d /r . %%d IN (dist) DO @IF EXIST "%%d" rd /s /q "%%d"
-
-
-
