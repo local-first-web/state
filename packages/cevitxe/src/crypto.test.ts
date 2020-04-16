39e5800f196053024c77a8abaf7b448e9b923cc1
Herb Caudill <herb@devresults.com>
2020-04-16 12:21:26 +0200

    cevitxe/crypto.test.ts:

--- packages/cevitxe/src/crypto.test.ts
+++ packages/cevitxe/src/crypto.test.ts
@@ -2,17 +2,35 @@ import { encrypt, decrypt } from './crypto'
 
 describe('crypto', () => {
   describe('encrypt/decrypt', () => {
-    test('round trip', async () => {
+    it('encrypts', () => {
+      const plaintext = 'Hello, world!'
+      const password = 'hello123'
+      const cipher = encrypt(plaintext, password)
+      expect(cipher).toHaveLength(24 + 48) // IV + ciphertext
+    })
+
+    it('decrypts', () => {
+      const password = 'hello123'
+      const cipher = 'y+Aig0DS8P+bn2DLY3fdUWGTQ5P7iZC1EUeBO+baAgUELfQ4JMjzjBokO2p+Ua5of7ECeGI='
+      const decrypted = decrypt(cipher, password)
+      expect(decrypted).toEqual('Hello, world!')
+    })
+
+    it(`throws an error when trying to decrypt with the wrong password`, () => {
+      const password = 'dontpwnmebro'
+      const cipher = 'y+Aig0DS8P+bn2DLY3fdUWGTQ5P7iZC1EUeBO+baAgUELfQ4JMjzjBokO2p+Ua5of7ECeGI='
+      expect(() => decrypt(cipher, password)).toThrow()
+    })
+
+    it('decrypts what it encrypts', () => {
       const plaintext = 'Hello, world!'
       const password = 'hello123'
 
       // encrypt
-      const cipher = await encrypt(plaintext, password)
-      expect(cipher).toHaveProperty('encrypted')
-      expect(cipher).toHaveProperty('iv')
+      const cipher = encrypt(plaintext, password)
 
       // decrypt
-      const decrypted = await decrypt(cipher, password)
+      const decrypted = decrypt(cipher, password)
       expect(decrypted).toEqual(plaintext)
     })
   })
