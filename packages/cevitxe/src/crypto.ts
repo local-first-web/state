1851b75f0f907f0a647a5947badad0145fec5495
Herb Caudill <herb@devresults.com>
2020-04-16 12:18:24 +0200

    cevitxe/crypto.ts: use tweetnacl

--- packages/cevitxe/src/crypto.ts
+++ packages/cevitxe/src/crypto.ts
@@ -1,61 +1,39 @@
-﻿import { subtle as crypto, getRandomValues } from 'isomorphic-webcrypto'
-import { encode, decode } from 'isomorphic-textencoder'
+﻿import { secretbox, randomBytes } from 'tweetnacl'
+import { decodeUTF8, encodeUTF8, encodeBase64, decodeBase64 } from 'tweetnacl-util'
+import scrypt from 'scryptsy'
 
-const JWK = 'jwk'
-const AES_GCM = 'AES-GCM'
-const AES_GCM_256 = { name: AES_GCM, length: 256 }
-const PBKDF2 = 'PBKDF2'
-const PBKDF2_256 = { name: PBKDF2, length: 256 }
-const ENCRYPT_DECRYPT = ['encrypt', 'decrypt']
-const BITS_KEY = ['deriveBits', 'deriveKey']
-const SHA256 = 'SHA-256'
+export const encrypt = (plaintext: string, password: string) => {
+  const key = deriveKey(password)
+  const keyUint8Array = decodeBase64(key)
 
-export const generateKey = () => crypto.generateKey(AES_GCM_256, true, ENCRYPT_DECRYPT)
+  const nonce = randomBytes(secretbox.nonceLength)
+  const message = decodeUTF8(plaintext)
+  const box = secretbox(message, nonce, keyUint8Array)
 
-export const exportKey = (key: CryptoKey) => crypto.exportKey(JWK, key)
+  const fullMessage = new Uint8Array(nonce.length + box.length)
+  fullMessage.set(nonce)
+  fullMessage.set(box, nonce.length)
 
-export const importKey = (jwk: JsonWebKey) =>
-  crypto.importKey(JWK, jwk, AES_GCM_256, true, ENCRYPT_DECRYPT)
-
-export const encrypt = async (plaintext: string, key: CryptoKey): Promise<Cipher> => {
-  const iv = getRandomValues(new Uint8Array(12))
-  const algo = { name: AES_GCM, iv }
-  const encryptedBytes = await crypto.encrypt(algo, key, encode(plaintext))
-  const encrypted = decode(encryptedBytes)
-  console.log({ encrypted })
-  return { encrypted, iv }
+  return encodeBase64(fullMessage)
 }
 
-export const decrypt = async ({ encrypted, iv }: Cipher, key: CryptoKey): Promise<string> => {
-  console.log({ encrypted, iv, key })
-  const algo = { name: AES_GCM, iv }
-  const decrypted = await crypto.decrypt(algo, key, encode(encrypted))
-  return decrypted.toString()
+export const decrypt = (cipher: string, password: string) => {
+  const key = deriveKey(password)
+  const keyUint8Array = decodeBase64(key)
+
+  const cipherbytes = decodeBase64(cipher)
+  const nonce = cipherbytes.slice(0, secretbox.nonceLength)
+  const message = cipherbytes.slice(secretbox.nonceLength, cipher.length)
+
+  const decrypted = secretbox.open(message, nonce, keyUint8Array)
+
+  if (!decrypted) throw new Error('Could not decrypt message')
+
+  return encodeUTF8(decrypted)
 }
 
-export const deriveKey = async (password: string) => {
-  const passphrase = encode(password.repeat(12 - password.length))
-  const key = await crypto.importKey('raw', passphrase, PBKDF2_256, false, BITS_KEY)
-  const saltText = await sha256('285a' + password + 'a9e2f694' + password.length)
-  const salt = encode(saltText)
-  const iterations = 100000 + password.length
-  const algo = { name: PBKDF2, salt, iterations, hash: SHA256 }
-
-  return crypto.deriveKey(algo, key, AES_GCM_256, true, ENCRYPT_DECRYPT)
-}
-
-export const sha256 = async (str: string) => {
-  const encoded = encode(str)
-  const digest = await crypto.digest(SHA256, encoded)
-  return hashBufferToHex(digest)
-}
-
-const hashBufferToHex = (hashBuffer: ArrayBuffer) =>
-  Array.from(new Uint8Array(hashBuffer))
-    .map(b => b.toString(16).padStart(2, '0'))
-    .join('')
-
-type Cipher = {
-  encrypted: string
-  iv: Uint8Array
+function deriveKey(password: string) {
+  const salt = 'Sõdìüm ÇhLôrɩdé'
+  const key = scrypt(password, salt, 16384, 8, 1, secretbox.keyLength)
+  return encodeBase64(key)
 }
