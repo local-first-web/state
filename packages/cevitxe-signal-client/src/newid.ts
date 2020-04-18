0e751340edb6786fe1417ee5cd140a3d3e80eb63
Herb Caudill <herb@devresults.com>
2020-04-18 09:58:57 +0200

    cevitxe-signal-client/newid.ts: refactor

--- packages/cevitxe-signal-client/src/newid.ts
+++ packages/cevitxe-signal-client/src/newid.ts
@@ -1,11 +1,13 @@
-﻿import cuid from 'cuid'
+﻿// ignore file coverage
+
+import cuid from 'cuid'
 // use shorter ids in development & testing
-export const newid = (len: number = 4) => {
-  return process.env.NODE_ENV === 'production'
-    ? cuid()
-    : cuid()
-        .split('')
-        .reverse()
-        .slice(0, len)
-        .join('') //the beginning of a cuid changes slowly, use the end
-}
+export const newid =
+  process.env.NODE_ENV === 'production'
+    ? cuid
+    : (len: number = 4) =>
+        cuid()
+          .split('')
+          .reverse()
+          .slice(0, len)
+          .join('') //the beginning of a cuid changes slowly, use the end
