eb22836ba7097ee54b7fe1caaf2c27f842c16d9f
Herb Caudill <herb@devresults.com>
2020-04-18 12:53:59 +0200

    cevitxe/crypto.test.ts:

--- packages/cevitxe/src/crypto.test.ts
+++ packages/cevitxe/src/crypto.test.ts
@@ -1,44 +1,44 @@
-import { decrypt, deriveKey, encrypt, sign, verify } from './crypto'
+import { symmetric, deriveKey, signatures, asymmetric } from './crypto'
+
+const plaintext = 'The leopard pounces at noon'
+const zalgoText = 'ẓ̴̇a̷̰̚l̶̥͑g̶̼͂o̴̅͜ ̸̻̏í̴͜s̵̜͠ ̴̦̃u̸̼̎p̵̘̔o̵̦͑ǹ̵̰ ̶̢͘u̵̇ͅș̷̏'
+const poop = '💩'
+const json = JSON.stringify(require('../package.json'))
 
 describe('crypto', () => {
   describe('deriveKey', () => {
     test('returns a 32-byte key', () => {
-      const password = 'secret'
+      const password = 'hello123'
       const key = deriveKey(password)
       expect(key).toHaveLength(32)
     })
   })
 
-  describe('encrypt/decrypt', () => {
-    const plaintext = 'The leopard pounces at noon'
+  describe('symmetric encrypt/decrypt', () => {
     const password = 'hello123'
+    const longPassword =
+      'eRPpBTwwa6gruOTq03AXGRFuR4WxBS6l6pGIiVlQPydVzEoWT2ST6Xqlyr1+XqYmmFEr4lFJ/u7l43RWd1Pxmaw2uG9pCCjw2JLnaRYcuy5XcwHRSEI4Cdcq+faT3GTvBtQGjWBmt7zPutPQIMB7Ii4fQyiB3mt67z2F68Y0Eph0/9JIwXeGfOVVQt347PCPCA6hgXe7+i4W6ylyIIuEQzJMQYGzu+l/2ktYOHxgY3pF0AL8yHuRPG30HFd6aSncAPyOBUyTI4qZ4S1IXJNLyBOq8pwNHdsxnxKw3QZarHniyoUEpvI+wMUpSRxZMHQOgaKBWH/wNFi3DgX3wfZdZTaPCD0G/1msL/e9ZtvXN9PApgULXJQmrWUO2chw5ptB8/xGd6h0Q3yDt8vcN/muWyoCSn91VlDIvSh1NOoEX08JEI1x8HJ7OE9TwrYJFFfgDAeV1D53IEcVkKe+5rtfjjwQBgfx+DA13DEee/ODaghvhX/venLSrFYWRk55gCc2BIyKATjYjLmLSHZ16Z98j09Ii93V7+E7XbzSuRqRQykzCRLRB0NoF7mQL46gRLF2gNXiqVqIUiY1u9rouxWZhSFDBeqWxWBjo3WJ7hZaaUTKSeInW6FTQuVqu4Tdn0/I3rwIhAAebVCftbakaWcjVsFsdyS8CLoiQBXg50VyriiFc2ckfyFs+AJBpndXkGOlg99EtXvRPzAR9NunGQxupCiRO3bnZG1xuKV3Y2iNc17O/WPDwqTIx/0CCUkZuDuJoiqhX+HJ6uyE6tS/7WBLCuVURirMkjBKNanWRe0KVopZgYVw2IEWbvO+PG7TO227llOSlI7mLwRb+QUSYtHKihp5xedTYAfq3l4Mdvt2D/6+lehhL/ijvieWs/RR9TMZE7eZcLwj8powELqMDYaL9H7pNZN0ha1AhOX+LTVWwmGS8K7f/s7XCgT+bl93SBRVfXbPA7LLkEy2PMHk3Xel0fn8zbGi+Z5I0QiNRFF0RTIXwBTZexV187Mkbv9J9ophj6vGs9TdeU5ByzWAVEgxwWhycBeamrKVoSypjowrftqAElL9OVdTAt6YMf/SuFW+dLm6acYcuzCWXs3kIEzDEJJc8TzLX2+CLk9yRmv1KGGicgDLvEE3mMbrtX18aaR6JP1j1zdwmfs5VgqA+Z93KR0m3OrXy4sYNVlI3ciTcU4jFO29FlzNTjre1Bl96apbX4ZJ6dY0c5GPFJastjgE9KY6/L5OD5NN2TaWWgAVh/Rv0wBLsu0vTWBMgLBoml0Tv4txtFDJKYKxflsNKLk053be0WQP10aOsEacuLxleP7eshDL/SmGk0vro3Np75bmLuybchS5Ns472y/5tRqZum3xekYi8DkAmVFCPzBkmq4TFUu9IxcjUapqIxFP6WqEI5vSOW6cgrENh983E5nQ8+wvAKoBFbzd6/aLmIyWqocGZvBl2/vs+XUvkOP3+aXHt/EJQrB0t6CdHyyMzUqGsCZoa/5JNFVH'
     const knownCipher =
       'drV+sao1EbVPG8S/Z+Z3gfaYBXoBiOYWIVVElGDtoNcZlWKtJsIq2AWBmjz3uZZgIExMAR+r8qqxfnsy40BPwM9zZQ=='
 
-    it('encrypts', () => {
-      const cipher = encrypt(plaintext, password)
+    test('alice encrypts using a password they both know', () => {
+      const cipher = symmetric.encrypt(plaintext, password)
       expect(cipher).toHaveLength(24 + 68) // IV + ciphertext
       expect(cipher).not.toEqual(knownCipher) // each encryption is different
     })
 
-    it('decrypts', () => {
-      expect(decrypt(knownCipher, password)).toEqual(plaintext)
+    test('bob decrypts using the password they both know', () => {
+      expect(symmetric.decrypt(knownCipher, password)).toEqual(plaintext)
     })
 
-    it(`throws an error when trying to decrypt with the wrong password`, () => {
+    test(`eve tries to decrypt but gets an error because she doesn't know the password`, () => {
       const wrongPassword = 'nachopassword'
-      expect(() => decrypt(knownCipher, wrongPassword)).toThrow()
+      expect(() => symmetric.decrypt(knownCipher, wrongPassword)).toThrow()
     })
-
-    const longPassword =
-      'eRPpBTwwa6gruOTq03AXGRFuR4WxBS6l6pGIiVlQPydVzEoWT2ST6Xqlyr1+XqYmmFEr4lFJ/u7l43RWd1Pxmaw2uG9pCCjw2JLnaRYcuy5XcwHRSEI4Cdcq+faT3GTvBtQGjWBmt7zPutPQIMB7Ii4fQyiB3mt67z2F68Y0Eph0/9JIwXeGfOVVQt347PCPCA6hgXe7+i4W6ylyIIuEQzJMQYGzu+l/2ktYOHxgY3pF0AL8yHuRPG30HFd6aSncAPyOBUyTI4qZ4S1IXJNLyBOq8pwNHdsxnxKw3QZarHniyoUEpvI+wMUpSRxZMHQOgaKBWH/wNFi3DgX3wfZdZTaPCD0G/1msL/e9ZtvXN9PApgULXJQmrWUO2chw5ptB8/xGd6h0Q3yDt8vcN/muWyoCSn91VlDIvSh1NOoEX08JEI1x8HJ7OE9TwrYJFFfgDAeV1D53IEcVkKe+5rtfjjwQBgfx+DA13DEee/ODaghvhX/venLSrFYWRk55gCc2BIyKATjYjLmLSHZ16Z98j09Ii93V7+E7XbzSuRqRQykzCRLRB0NoF7mQL46gRLF2gNXiqVqIUiY1u9rouxWZhSFDBeqWxWBjo3WJ7hZaaUTKSeInW6FTQuVqu4Tdn0/I3rwIhAAebVCftbakaWcjVsFsdyS8CLoiQBXg50VyriiFc2ckfyFs+AJBpndXkGOlg99EtXvRPzAR9NunGQxupCiRO3bnZG1xuKV3Y2iNc17O/WPDwqTIx/0CCUkZuDuJoiqhX+HJ6uyE6tS/7WBLCuVURirMkjBKNanWRe0KVopZgYVw2IEWbvO+PG7TO227llOSlI7mLwRb+QUSYtHKihp5xedTYAfq3l4Mdvt2D/6+lehhL/ijvieWs/RR9TMZE7eZcLwj8powELqMDYaL9H7pNZN0ha1AhOX+LTVWwmGS8K7f/s7XCgT+bl93SBRVfXbPA7LLkEy2PMHk3Xel0fn8zbGi+Z5I0QiNRFF0RTIXwBTZexV187Mkbv9J9ophj6vGs9TdeU5ByzWAVEgxwWhycBeamrKVoSypjowrftqAElL9OVdTAt6YMf/SuFW+dLm6acYcuzCWXs3kIEzDEJJc8TzLX2+CLk9yRmv1KGGicgDLvEE3mMbrtX18aaR6JP1j1zdwmfs5VgqA+Z93KR0m3OrXy4sYNVlI3ciTcU4jFO29FlzNTjre1Bl96apbX4ZJ6dY0c5GPFJastjgE9KY6/L5OD5NN2TaWWgAVh/Rv0wBLsu0vTWBMgLBoml0Tv4txtFDJKYKxflsNKLk053be0WQP10aOsEacuLxleP7eshDL/SmGk0vro3Np75bmLuybchS5Ns472y/5tRqZum3xekYi8DkAmVFCPzBkmq4TFUu9IxcjUapqIxFP6WqEI5vSOW6cgrENh983E5nQ8+wvAKoBFbzd6/aLmIyWqocGZvBl2/vs+XUvkOP3+aXHt/EJQrB0t6CdHyyMzUqGsCZoa/5JNFVH'
-    const zalgoText = 'ẓ̴̇a̷̰̚l̶̥͑g̶̼͂o̴̅͜ ̸̻̏í̴͜s̵̜͠ ̴̦̃u̸̼̎p̵̘̔o̵̦͑ǹ̵̰ ̶̢͘u̵̇ͅș̷̏'
-    const poop = '💩'
-    const json = JSON.stringify(require('../package.json'))
 
     test.each`
-      label                 | s            | pw
-      ${'hello world'}      | ${plaintext} | ${password}
+      label                 | message      | password
+      ${'plain text'}       | ${plaintext} | ${password}
       ${'empty string'}     | ${''}        | ${password}
       ${'emoji message'}    | ${poop}      | ${password}
       ${'stringified json'} | ${json}      | ${password}
@@ -47,43 +47,124 @@ describe('crypto', () => {
       ${'emoji password'}   | ${plaintext} | ${poop}
       ${'long password'}    | ${plaintext} | ${longPassword}
       ${'zalgo password'}   | ${plaintext} | ${zalgoText}
-    `('decrypts what it encrypts: $label', ({ s, pw }) => {
-      const cipher = encrypt(s, pw)
-      expect(decrypt(cipher, pw)).toEqual(s)
+    `('round trip: $label', ({ message, password }) => {
+      const cipher = symmetric.encrypt(message, password)
+      expect(symmetric.decrypt(cipher, password)).toEqual(message)
     })
   })
 
-  describe('sign', () => {
-    const message = 'One if by land, two if by sea'
-    const publicKey = 'OH8olQvUFfxqjd+A4FkPQZq0mSb9GGKIOfuCFLDd0B0='
-    const secretKey =
-      'TVTqqajwDkAMlztAJEkgcnEd1KzWheaDQE6sxPGlUlY4fyiVC9QV/GqN34DgWQ9BmrSZJv0YYog5+4IUsN3QHQ=='
-    const signature =
-      '/SO4d59oLiCkEG57ZubXLsMaaNzk91ujZjXkS9doP2vCAFimKvKdgjyyPJd9FrjyzqLQwa25AUyJlhN8veVIBQ=='
+  describe('asymmetric encrypt/decrypt', () => {
+    const alice = {
+      publicKey: 'uUcxiVUXq8LnLVS5yjmfIdd2ZAuWIWfM4IVeuXqSyUY=',
+      secretKey: 'wOyhsgjAdHPo8oPew31wyMJo+1ckA0zQoebgtN8hK9U=',
+    }
 
-    it('signs', () => {
-      const signature = sign(message, secretKey)
-      expect(signature).toEqual(signature)
+    const bob = {
+      publicKey: 'QwqgJTVqB1hxdMxgY42lQT9gss1SUrUtfh45wZzc33g=',
+      secretKey: 'paGP2tYzpV8kYzeD5dJtcmB1o16N1uh9eBadkvHktnY=',
+    }
+
+    const eve = {
+      publicKey: 'jVj8N4+zpxIoVLlpu0KA7Ai8OR8I4QhnS64WMhQk4R0=',
+      secretKey: 'qiSxzj2NgUt5YUj6PccfiEjYQI8wIlPgWqs9HeaBEbs=',
+    }
+
+    const knownCipher =
+      '0UllJr2FBwolmAGHg0FUuAfpweLyUSgYT74U/RH6FeEiDw64zFxvFeLJd6LX0D/YMYxj1aNwRmy5LapQIyh1QnKLuQ=='
+
+    test(`alice encrypts using her secret key and bob's public key`, () => {
+      const cipherFromAlice = asymmetric.encrypt(plaintext, bob.publicKey, alice.secretKey)
+      expect(cipherFromAlice).toHaveLength(24 + 68) // IV + ciphertext
+      expect(cipherFromAlice).not.toEqual(knownCipher) // each encryption is different
     })
 
-    it('verifies', () => {
-      expect(verify(message, signature, publicKey)).toBe(true)
+    test(`bob decrypts using his secret key and alice's public key`, () => {
+      const cipherFromAlice = knownCipher
+      expect(asymmetric.decrypt(cipherFromAlice, alice.publicKey, bob.secretKey)).toEqual(plaintext)
     })
 
-    it(`fails verification if message has changed`, () => {
-      const badMessage = 'Twenty-seven if by land, fourteen if by sea'
-      expect(verify(badMessage, signature, publicKey)).toBe(false)
+    test(`eve can't decrypt with her secret key`, () => {
+      const cipherFromAlice = knownCipher
+      expect(() => asymmetric.decrypt(cipherFromAlice, alice.publicKey, eve.secretKey)).toThrow()
     })
 
-    it(`fails verification if signature is wrong`, () => {
+    test(`can't decrypt with the wrong public key`, () => {
+      const cipherFromAlice = knownCipher
+      expect(() => asymmetric.decrypt(cipherFromAlice, eve.publicKey, bob.secretKey)).toThrow()
+    })
+
+    test.each`
+      label                 | message
+      ${'plain text'}       | ${plaintext}
+      ${'empty string'}     | ${''}
+      ${'emoji message'}    | ${poop}
+      ${'stringified json'} | ${json}
+      ${'zalgo text'}       | ${zalgoText}
+    `('round trip: $label', ({ message }) => {
+      const encrypted = asymmetric.encrypt(message, bob.publicKey, alice.secretKey)
+      const decrypted = asymmetric.decrypt(encrypted, alice.publicKey, bob.secretKey)
+      expect(decrypted).toEqual(message)
+    })
+
+    test('fwiw: cannot use signature keys to encrypt', () => {
+      const a = signatures.keyPair()
+      const b = signatures.keyPair()
+      expect(() => asymmetric.encrypt(plaintext, b.publicKey, a.secretKey)).toThrow()
+    })
+  })
+
+  describe('signatures', () => {
+    const alice = {
+      publicKey: 'OH8olQvUFfxqjd+A4FkPQZq0mSb9GGKIOfuCFLDd0B0=',
+      secretKey:
+        'TVTqqajwDkAMlztAJEkgcnEd1KzWheaDQE6sxPGlUlY4fyiVC9QV/GqN34DgWQ9BmrSZJv0YYog5+4IUsN3QHQ==',
+    }
+
+    const signedMessage = {
+      contents: 'one if by day, two if by night',
+      signature:
+        'Qd9f/Xgk9QFG9nVNb/QkHqKTNF0JQCEy848m4w8UmxSRwnuomBZz6Bi8wDopz//iKwHq3ipMvA2AGAw8Oo19Dw==',
+    }
+
+    test('alice signs with her secret key', () => {
+      const { contents, signature: knownSignature } = signedMessage
+      const signature = signatures.sign(contents, alice.secretKey)
+      expect(signature).toEqual(knownSignature)
+    })
+
+    test(`bob verifies using alice's public key`, () => {
+      const { contents, signature } = signedMessage
+      const result = signatures.verify(contents, signature, alice.publicKey)
+      expect(result).toBe(true)
+    })
+
+    test(`eve tampers with the message, but bob is not fooled`, () => {
+      const tamperedMessage = {
+        ...signedMessage,
+        contents: signedMessage.contents
+          .replace('one', 'forty-two')
+          .replace('two', 'seventy-twelve'),
+      }
+      const { contents, signature } = tamperedMessage
+      const result = signatures.verify(contents, signature, alice.publicKey)
+      expect(result).toBe(false)
+    })
+
+    test(`fails verification if signature is wrong`, () => {
       const badSignature =
         'Iamabadbadsignature+JlhN8veVIBQ/SO4d59oLiCkEG57ZubXLsMaaNzk91ujZjXkS9doP2vCAFimKvKdgjy=='
-      expect(verify(message, badSignature, publicKey)).toBe(false)
+      expect(signatures.verify(plaintext, badSignature, alice.publicKey)).toBe(false)
     })
 
-    it(`fails verification if public key is wrong`, () => {
+    test(`fails verification if public key is wrong`, () => {
       const badKey = 'NachoKeySb9GGKIOfuCFLDd0B0OH8olQvUFfxqjd+A4='
-      expect(verify(message, signature, badKey)).toBe(false)
+      const { contents, signature } = signedMessage
+      expect(signatures.verify(contents, signature, badKey)).toBe(false)
+    })
+
+    test('fwiw: cannot use encryption keys to sign', () => {
+      const a = asymmetric.keyPair()
+      expect(() => signatures.sign(plaintext, a.secretKey)).toThrow()
     })
   })
 })
