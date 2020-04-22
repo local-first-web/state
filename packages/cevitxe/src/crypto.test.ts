282afb652e542377ac36f7f8f46a41e7e91c95a2
Herb Caudill <herb@devresults.com>
2020-04-22 14:59:52 +0200

    Basic crypto library (#68)

---
+++ packages/cevitxe/src/crypto.test.ts
@@ -0,0 +1,193 @@
+import * as crypto from './crypto'
+
+const plaintext = 'The leopard pounces at noon'
+const zalgoText = 'ẓ̴̇a̷̰̚l̶̥͑g̶̼͂o̴̅͜ ̸̻̏í̴͜s̵̜͠ ̴̦̃u̸̼̎p̵̘̔o̵̦͑ǹ̵̰ ̶̢͘u̵̇ͅș̷̏'
+const poop = '💩'
+const json = JSON.stringify(require('../package.json'))
+
+describe('crypto', () => {
+  describe('deriveKey', () => {
+    test('returns a 32-byte key', () => {
+      const password = 'hello123'
+      const key = crypto.deriveKey(password)
+      expect(key).toHaveLength(32)
+    })
+  })
+
+  describe('symmetric encrypt/decrypt', () => {
+    const { encrypt, decrypt } = crypto.symmetric
+
+    const password = 'hello123'
+    const longPassword =
+      'eRPpBTwwa6gruOTq03AXGRFuR4WxBS6l6pGIiVlQPydVzEoWT2ST6Xqlyr1+XqYmmFEr4lFJ/u7l43RWd1Pxmaw2uG9pCCjw2JLnaRYcuy5XcwHRSEI4Cdcq+faT3GTvBtQGjWBmt7zPutPQIMB7Ii4fQyiB3mt67z2F68Y0Eph0/9JIwXeGfOVVQt347PCPCA6hgXe7+i4W6ylyIIuEQzJMQYGzu+l/2ktYOHxgY3pF0AL8yHuRPG30HFd6aSncAPyOBUyTI4qZ4S1IXJNLyBOq8pwNHdsxnxKw3QZarHniyoUEpvI+wMUpSRxZMHQOgaKBWH/wNFi3DgX3wfZdZTaPCD0G/1msL/e9ZtvXN9PApgULXJQmrWUO2chw5ptB8/xGd6h0Q3yDt8vcN/muWyoCSn91VlDIvSh1NOoEX08JEI1x8HJ7OE9TwrYJFFfgDAeV1D53IEcVkKe+5rtfjjwQBgfx+DA13DEee/ODaghvhX/venLSrFYWRk55gCc2BIyKATjYjLmLSHZ16Z98j09Ii93V7+E7XbzSuRqRQykzCRLRB0NoF7mQL46gRLF2gNXiqVqIUiY1u9rouxWZhSFDBeqWxWBjo3WJ7hZaaUTKSeInW6FTQuVqu4Tdn0/I3rwIhAAebVCftbakaWcjVsFsdyS8CLoiQBXg50VyriiFc2ckfyFs+AJBpndXkGOlg99EtXvRPzAR9NunGQxupCiRO3bnZG1xuKV3Y2iNc17O/WPDwqTIx/0CCUkZuDuJoiqhX+HJ6uyE6tS/7WBLCuVURirMkjBKNanWRe0KVopZgYVw2IEWbvO+PG7TO227llOSlI7mLwRb+QUSYtHKihp5xedTYAfq3l4Mdvt2D/6+lehhL/ijvieWs/RR9TMZE7eZcLwj8powELqMDYaL9H7pNZN0ha1AhOX+LTVWwmGS8K7f/s7XCgT+bl93SBRVfXbPA7LLkEy2PMHk3Xel0fn8zbGi+Z5I0QiNRFF0RTIXwBTZexV187Mkbv9J9ophj6vGs9TdeU5ByzWAVEgxwWhycBeamrKVoSypjowrftqAElL9OVdTAt6YMf/SuFW+dLm6acYcuzCWXs3kIEzDEJJc8TzLX2+CLk9yRmv1KGGicgDLvEE3mMbrtX18aaR6JP1j1zdwmfs5VgqA+Z93KR0m3OrXy4sYNVlI3ciTcU4jFO29FlzNTjre1Bl96apbX4ZJ6dY0c5GPFJastjgE9KY6/L5OD5NN2TaWWgAVh/Rv0wBLsu0vTWBMgLBoml0Tv4txtFDJKYKxflsNKLk053be0WQP10aOsEacuLxleP7eshDL/SmGk0vro3Np75bmLuybchS5Ns472y/5tRqZum3xekYi8DkAmVFCPzBkmq4TFUu9IxcjUapqIxFP6WqEI5vSOW6cgrENh983E5nQ8+wvAKoBFbzd6/aLmIyWqocGZvBl2/vs+XUvkOP3+aXHt/EJQrB0t6CdHyyMzUqGsCZoa/5JNFVH'
+    const knownCipher =
+      'drV+sao1EbVPG8S/Z+Z3gfaYBXoBiOYWIVVElGDtoNcZlWKtJsIq2AWBmjz3uZZgIExMAR+r8qqxfnsy40BPwM9zZQ=='
+
+    test('alice encrypts using a password they both know', () => {
+      const cipher = encrypt(plaintext, password)
+      expect(cipher).toHaveLength(24 + 68) // nonce + ciphertext
+      expect(cipher).not.toEqual(knownCipher) // each encryption is different
+    })
+
+    test('bob decrypts using the password they both know', () => {
+      expect(decrypt(knownCipher, password)).toEqual(plaintext)
+    })
+
+    test(`eve tries to decrypt but gets an error because she doesn't know the password`, () => {
+      const wrongPassword = 'nachopassword'
+      expect(() => decrypt(knownCipher, wrongPassword)).toThrow()
+    })
+
+    test.each`
+      label                 | message      | password
+      ${'plain text'}       | ${plaintext} | ${password}
+      ${'empty string'}     | ${''}        | ${password}
+      ${'emoji message'}    | ${poop}      | ${password}
+      ${'stringified json'} | ${json}      | ${password}
+      ${'zalgo text'}       | ${zalgoText} | ${password}
+      ${'empty password'}   | ${plaintext} | ${''}
+      ${'emoji password'}   | ${plaintext} | ${poop}
+      ${'long password'}    | ${plaintext} | ${longPassword}
+      ${'zalgo password'}   | ${plaintext} | ${zalgoText}
+    `('round trip: $label', ({ message, password }) => {
+      const cipher = encrypt(message, password)
+      expect(decrypt(cipher, password)).toEqual(message)
+
+      const attemptToDecrypt = () => decrypt(cipher, 'nachopassword')
+      expect(attemptToDecrypt).toThrow()
+    })
+  })
+
+  describe('asymmetric encrypt/decrypt', () => {
+    const { encrypt, decrypt } = crypto.asymmetric
+
+    const alice = {
+      publicKey: 'uUcxiVUXq8LnLVS5yjmfIdd2ZAuWIWfM4IVeuXqSyUY=',
+      secretKey: 'wOyhsgjAdHPo8oPew31wyMJo+1ckA0zQoebgtN8hK9U=',
+    }
+
+    const bob = {
+      publicKey: 'QwqgJTVqB1hxdMxgY42lQT9gss1SUrUtfh45wZzc33g=',
+      secretKey: 'paGP2tYzpV8kYzeD5dJtcmB1o16N1uh9eBadkvHktnY=',
+    }
+
+    const eve = {
+      publicKey: 'jVj8N4+zpxIoVLlpu0KA7Ai8OR8I4QhnS64WMhQk4R0=',
+      secretKey: 'qiSxzj2NgUt5YUj6PccfiEjYQI8wIlPgWqs9HeaBEbs=',
+    }
+
+    const knownCipher =
+      '0UllJr2FBwolmAGHg0FUuAfpweLyUSgYT74U/RH6FeEiDw64zFxvFeLJd6LX0D/YMYxj1aNwRmy5LapQIyh1QnKLuQ=='
+
+    test(`alice encrypts using her secret key and bob's public key`, () => {
+      const cipherFromAlice = encrypt(plaintext, bob.publicKey, alice.secretKey)
+      expect(cipherFromAlice).toHaveLength(24 + 68) // IV + ciphertext
+      expect(cipherFromAlice).not.toEqual(knownCipher) // each encryption is different
+    })
+
+    test(`bob decrypts using his secret key and alice's public key`, () => {
+      const cipherFromAlice = knownCipher
+      expect(decrypt(cipherFromAlice, alice.publicKey, bob.secretKey)).toEqual(plaintext)
+    })
+
+    test(`eve can't decrypt with her secret key`, () => {
+      const cipherFromAlice = knownCipher
+      const attemptToDecrypt = () => decrypt(cipherFromAlice, alice.publicKey, eve.secretKey)
+      expect(attemptToDecrypt).toThrow()
+    })
+
+    test(`can't decrypt with the wrong public key`, () => {
+      const cipherFromAlice = knownCipher
+      const attemptToDecrypt = () => decrypt(cipherFromAlice, eve.publicKey, bob.secretKey)
+      expect(attemptToDecrypt).toThrow()
+    })
+
+    test.each`
+      label                 | message
+      ${'plain text'}       | ${plaintext}
+      ${'empty string'}     | ${''}
+      ${'emoji message'}    | ${poop}
+      ${'stringified json'} | ${json}
+      ${'zalgo text'}       | ${zalgoText}
+    `('round trip: $label', ({ message }) => {
+      const encrypted = encrypt(message, bob.publicKey, alice.secretKey)
+      const decrypted = decrypt(encrypted, alice.publicKey, bob.secretKey)
+      expect(decrypted).toEqual(message)
+
+      const attemptToDecrypt = () => decrypt(encrypted, alice.publicKey, eve.secretKey)
+      expect(attemptToDecrypt).toThrow()
+    })
+
+    test('fwiw: cannot use signature keys to encrypt', () => {
+      const a = crypto.signatures.keyPair()
+      const b = crypto.signatures.keyPair()
+      expect(() => encrypt(plaintext, b.publicKey, a.secretKey)).toThrow()
+    })
+  })
+
+  describe('signatures', () => {
+    const { sign, verify } = crypto.signatures
+
+    const alice = {
+      publicKey: 'OH8olQvUFfxqjd+A4FkPQZq0mSb9GGKIOfuCFLDd0B0=',
+      secretKey:
+        'TVTqqajwDkAMlztAJEkgcnEd1KzWheaDQE6sxPGlUlY4fyiVC9QV/GqN34DgWQ9BmrSZJv0YYog5+4IUsN3QHQ==',
+    }
+
+    const signedMessage: crypto.SignedMessage = {
+      content: 'one if by day, two if by night',
+      signature:
+        'Qd9f/Xgk9QFG9nVNb/QkHqKTNF0JQCEy848m4w8UmxSRwnuomBZz6Bi8wDopz//iKwHq3ipMvA2AGAw8Oo19Dw==',
+      publicKey: alice.publicKey,
+    }
+
+    test('alice signs with her secret key', () => {
+      const { content, signature: knownSignature } = signedMessage
+      const signature = sign(content, alice.secretKey)
+      expect(signature).toEqual(knownSignature)
+    })
+
+    test(`bob verifies using alice's public key`, () => {
+      const isLegit = verify(signedMessage)
+      expect(isLegit).toBe(true)
+    })
+
+    test(`eve tampers with the message, but bob is not fooled`, () => {
+      const tamperedContent = signedMessage.content
+        .replace('one', 'forty-two')
+        .replace('two', 'seventy-twelve')
+      const tamperedMessage = {
+        ...signedMessage,
+        content: tamperedContent,
+      }
+      const isLegit = verify(tamperedMessage)
+      expect(isLegit).toBe(false)
+    })
+
+    test(`fails verification if signature is wrong`, () => {
+      const badSignature =
+        'Iamabadbadsignature+JlhN8veVIBQ/SO4d59oLiCkEG57ZubXLsMaaNzk91ujZjXkS9doP2vCAFimKvKdgjy=='
+      const badMessage = {
+        ...signedMessage,
+        signature: badSignature,
+      }
+      const isLegit = verify(badMessage)
+      expect(isLegit).toBe(false)
+    })
+
+    test(`fails verification if public key is wrong`, () => {
+      const badKey = 'NachoKeySb9GGKIOfuCFLDd0B0OH8olQvUFfxqjd+A4='
+      const badMessage = {
+        ...signedMessage,
+        publicKey: badKey,
+      }
+      const isLegit = verify(badMessage)
+      expect(isLegit).toBe(false)
+    })
+
+    test('fwiw: cannot use encryption keys to sign', () => {
+      const a = crypto.asymmetric.keyPair()
+      expect(() => sign(plaintext, a.secretKey)).toThrow()
+    })
+  })
+})
---
+++ packages/cevitxe/src/crypto.ts
@@ -0,0 +1,206 @@
+﻿import * as base64 from '@stablelib/base64'
+import * as utf8 from '@stablelib/utf8'
+import memoize from 'fast-memoize'
+import scrypt from 'scryptsy'
+import nacl from 'tweetnacl'
+
+// These are straightforward implementations of NaCl crypto functions, accepting and returning
+// base64 strings rather than byte arrays. The symmetric `encrypt` and `decrypt` functions can take
+// passwords instead of 32-byte keys; the password is expanded using the
+// [scrypt](https://en.wikipedia.org/wiki/Scrypt) algorithm.
+
+export const symmetric = {
+  /**
+   * Symmetrically encrypts a string of text.
+   * @param plaintext The plaintext to encrypt
+   * @param password The password to use as a seed for an encryption key
+   * @returns The encrypted data, encoded as a base64 string. The first 24 characters are the nonce;
+   * the rest of the string is the encrypted message.
+   * @see symmetric.decrypt
+   */
+  encrypt: (plaintext: string, password: string) => {
+    const key = deriveKey(password)
+
+    const nonce = newNonce()
+    const message = utf8.encode(plaintext)
+    const box = nacl.secretbox(message, nonce, key)
+
+    const cipherBytes = new Uint8Array(nonceLength + box.length)
+    // the first 24 characters are the nonce
+    cipherBytes.set(nonce)
+    // the rest is the message
+    cipherBytes.set(box, nonceLength)
+
+    return base64.encode(cipherBytes)
+  },
+
+  /**
+   * Symmetrically decrypts a message encrypted by `symmetric.encrypt`.
+   * @param cipher The encrypted data, encoded as a base64 string (the first 24 characters are the nonce;
+   * the rest of the string is the encrypted message)
+   * @param password The password used as a seed for an encryption key
+   * @returns The original plaintext
+   * @see symmetric.encrypt
+   */
+  decrypt: (cipher: string, password: string) => {
+    const key = deriveKey(password)
+    const cipherBytes = base64.decode(cipher)
+
+    // the first 24 characters are the nonce
+    const nonce = cipherBytes.slice(0, nonceLength)
+    // the rest is the message
+    const message = cipherBytes.slice(nonceLength, cipher.length)
+
+    const decrypted = nacl.secretbox.open(message, nonce, key)
+    if (!decrypted) throw new Error('Could not decrypt message')
+
+    return utf8.decode(decrypted)
+  },
+}
+
+export const asymmetric = {
+  /**
+   * @returns A key pair consisting of a public key and a secret key, encoded as base64 strings, to
+   * use for asymmetric encryption and decryption. (Note that asymmetric encryption keys cannot be
+   * used for signatures, and vice versa.)
+   */
+  keyPair: () => {
+    const keys = nacl.box.keyPair()
+    return {
+      publicKey: base64.encode(keys.publicKey),
+      secretKey: base64.encode(keys.secretKey),
+    }
+  },
+
+  /**
+   * Asymmetrically encrypts a string of text.
+   * @param plaintext The plaintext to encrypt
+   * @param recipientPublicKey The public key of the intended recipient
+   * @param senderSecretKey The secret key of the sender
+   * @returns The encrypted data, encoded as a base64 string. The first 24 characters are the nonce;
+   * the rest of the string is the encrypted message.
+   * @see asymmetric.decrypt
+   */
+  encrypt: (plaintext: string, recipientPublicKey: string, senderSecretKey: string) => {
+    const nonce = newNonce()
+    const messageBytes = utf8.encode(plaintext)
+    const encrypted = nacl.box(
+      messageBytes,
+      nonce,
+      base64.decode(recipientPublicKey),
+      base64.decode(senderSecretKey)
+    )
+
+    const cipherBytes = new Uint8Array(nonceLength + encrypted.length)
+    // the first 24 characters are the nonce
+    cipherBytes.set(nonce)
+    // the rest is the message
+    cipherBytes.set(encrypted, nonceLength)
+
+    return base64.encode(cipherBytes)
+  },
+
+  /**
+   * Asymmetrically decrypts a message encrypted by `asymmetric.encrypt`.
+   * @param cipher The encrypted data, encoded as a base64 string (the first 24 characters are the nonce;
+   * the rest of the string is the encrypted message)
+   * @param senderPublicKey The public key of the sender
+   * @param recipientSecretKey The secret key of the recipient
+   * @returns The original plaintext
+   * @see asymmetric.encrypt
+   */
+  decrypt: (cipher: string, senderPublicKey: string, recipientSecretKey: string) => {
+    const cipherBytes = base64.decode(cipher)
+
+    // the first 24 characters are the nonce
+    const nonce = cipherBytes.slice(0, nonceLength)
+    // the rest is the message
+    const message = cipherBytes.slice(nonceLength, cipher.length)
+
+    const decrypted = nacl.box.open(
+      message,
+      nonce,
+      base64.decode(senderPublicKey),
+      base64.decode(recipientSecretKey)
+    )
+
+    if (!decrypted) throw new Error('Could not decrypt message')
+
+    return utf8.decode(decrypted)
+  },
+}
+
+export const signatures = {
+  /**
+   * @returns A key pair consisting of a public key and a secret key, encoded as base64 strings, to
+   * use for signing and verifying messages. (Note that signature keys cannot be used for asymmetric
+   * encryption, and vice versa.)
+   */
+  keyPair: () => {
+    const keys = nacl.sign.keyPair()
+    return {
+      publicKey: base64.encode(keys.publicKey),
+      secretKey: base64.encode(keys.secretKey),
+    }
+  },
+
+  /**
+   * @param message The plaintext message to sign
+   * @param secretKey The signer's secret key, encoded as a base64 string
+   * @returns A signature, encoded as a base64 string
+   */
+  sign: (message: string, secretKey: string) =>
+    base64.encode(
+      nacl.sign.detached(
+        utf8.encode(message), //
+        base64.decode(secretKey)
+      )
+    ),
+
+  /**
+   * @param content The plaintext message to be verified
+   * @param signature The signature provided along with the message, encoded as a base64 string
+   * @param publicKey The signer's public key, encoded as a base64 string
+   * @returns true if verification succeeds, false otherwise
+   */
+  verify: ({ content, signature, publicKey }: SignedMessage) =>
+    nacl.sign.detached.verify(
+      utf8.encode(content),
+      base64.decode(signature),
+      base64.decode(publicKey)
+    ),
+}
+
+/**
+ * Uses the `scrypt` algorithm to deterministically derive a 32-byte key from the password provided.
+ * @password The password to use as a seed
+ * @returns A 32-byte secret key to use for symmetric encryption
+ */
+export const deriveKey = memoize((password: string) => {
+  const salt = 'Sõdìüm ÇhLôrɩdé'
+
+  // `scrypt` is intended to be expensive not only in CPU time but in memory usage.
+  // These parameters are calibrated to keep the derivation time around 100ms "on my box"
+  // as per author recommendations. See http://tarsnap.com/scrypt/scrypt.pdf
+  const blockSizeFactor = 8
+  const costFactor = 2 ** 11
+  const parallelizationFactor = 1
+
+  return scrypt(
+    password,
+    salt,
+    costFactor,
+    blockSizeFactor,
+    parallelizationFactor,
+    nacl.secretbox.keyLength // = 32
+  )
+})
+
+const nonceLength = nacl.box.nonceLength // = 24
+const newNonce = () => nacl.randomBytes(nonceLength)
+
+export type SignedMessage = {
+  content: string
+  signature: string
+  publicKey: string
+}
