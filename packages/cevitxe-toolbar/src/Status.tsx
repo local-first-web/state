ad2537e3abc27624effcb13150cf0950d04ad5ee
Herb Caudill <herb@devresults.com>
2020-06-13 16:21:58 +0200

    cevitxe-toolbar/Status.tsx: use StatusLight

--- packages/cevitxe-toolbar/src/Status.tsx
+++ packages/cevitxe-toolbar/src/Status.tsx
@@ -1,8 +1,15 @@
-import React, { Component } from 'react'
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
 import { StoreManager } from 'cevitxe'
+import { Component, Fragment } from 'react'
+import { StatusLight } from './StatusLight'
+import { styles } from './Toolbar'
+import { ConnectionEvent } from 'cevitxe-types'
+
+const { OPEN, CLOSE, PEER, PEER_REMOVE } = ConnectionEvent
 
 interface StatusProps<T> {
-  storeManager: StoreManager<T>;
+  storeManager: StoreManager<T>
 }
 
 interface StatusState {
@@ -10,7 +17,6 @@ interface StatusState {
 }
 
 export class Status extends Component<StatusProps<any>, StatusState> {
-
   constructor(props: StatusProps<any>) {
     super(props)
     this.state = { peers: undefined }
@@ -20,37 +26,51 @@ export class Status extends Component<StatusProps<any>, StatusState> {
   }
 
   peerHandler(peers: String[]) {
-    this.setState({peers})
+    this.setState({ peers })
   }
+
   openHandler() {
-    this.setState({peers: []})
+    this.setState({ peers: [] })
   }
+
   closeHandler() {
-    this.setState({peers: undefined})
+    this.setState({ peers: undefined })
   }
+
   componentDidMount() {
     const m = this.props.storeManager
-    m.on('open', this.openHandler)
-    m.on('close', this.closeHandler)
-    m.on('peer_add', this.peerHandler)
-    m.on('peer_remove', this.peerHandler)
+    m.on(OPEN, this.openHandler)
+    m.on(CLOSE, this.closeHandler)
+    m.on(PEER, this.peerHandler)
+    m.on(PEER_REMOVE, this.peerHandler)
   }
+
   componentWillUnmount() {
     const m = this.props.storeManager
-    m.off('open', this.openHandler)
-    m.off('close', this.closeHandler)
-    m.off('peer_add', this.peerHandler)
-    m.off('peer_remove', this.peerHandler)
+    m.off(OPEN, this.openHandler)
+    m.off(CLOSE, this.closeHandler)
+    m.off(PEER, this.peerHandler)
+    m.off(PEER_REMOVE, this.peerHandler)
   }
 
   render() {
     const { peers } = this.state
-    if (!peers) {
-        return <p>Not connected to internet ring.</p>
-    } else if (peers.length > 0) {
-        return <p>Connected to {peers.length} other user(s)</p>
-    } else {
-        return <p>Connected but there are no other users</p>
-    }
+    return (
+      <div
+        css={styles.toolbarGroup}
+        title={
+          !peers
+            ? 'offline'
+            : `online; ${peers.length} other ${
+                peers.length === 1 ? 'peer is' : 'peers are'
+              } connected`
+        }
+      >
+        <label>
+          <StatusLight connected={!!peers} />
+          {peers && <Fragment>{peers.length}</Fragment>}
+        </label>
+      </div>
+    )
   }
 }
