5b35f49d11f3a62f8aa3337da30012f15624ac95
Herb Caudill <herb@devresults.com>
2020-06-13 16:13:04 +0200

    cevitxe-toolbar/StatusLight.tsx: create

---
+++ packages/cevitxe-toolbar/src/StatusLight.tsx
@@ -0,0 +1,23 @@
+﻿/** @jsx jsx */
+import { jsx } from '@emotion/core'
+
+export const StatusLight = ({ connected }: { connected: boolean }) => (
+  <div css={styles(connected).statusLight}>
+    <span></span>
+  </div>
+)
+
+const styles = (connected: boolean) => ({
+  statusLight: {
+    display: 'inline-block',
+    marginRight: '1em',
+    span: {
+      display: 'block',
+      width: 10,
+      height: 10,
+      borderRadius: '50%',
+      border: '1px solid white',
+      background: connected ? 'green' : 'silver',
+    },
+  },
+})
