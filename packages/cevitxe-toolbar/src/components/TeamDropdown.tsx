24e1c689ba93c0033490dc08dea00b80ac63663c
Herb Caudill <herb@devresults.com>
2020-09-04 19:01:01 +0200

    cevitxe-toolbar/components/TeamDropdown.tsx:

--- packages/cevitxe-toolbar/src/components/TeamDropdown.tsx
+++ packages/cevitxe-toolbar/src/components/TeamDropdown.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button } from './Button'
-import { Group } from './Group'
 import { DropdownWrapper } from './DropdownWrapper'
+import { Group } from './Group'
 import { Item } from './Item'
 
 export const TeamDropdown: React.FunctionComponent<any> = () => {
