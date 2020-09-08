4c4a2cf3d43cf92f63830d81153ad979281dcaad
Herb Caudill <herb@devresults.com>
2020-09-08 17:04:56 +0200

    cevitxe-toolbar/lib/localUser.ts:

--- packages/cevitxe-toolbar/src/lib/localUser.ts
+++ packages/cevitxe-toolbar/src/lib/localUser.ts
@@ -1,9 +1,4 @@
-﻿import { user, team } from 'taco-js'
+﻿import { createUser, loadUser } from 'taco-js'
 import { randomUserName } from './randomName'
 
-const createUser = () => {
-  const userName = randomUserName()
-  return user.create(userName)
-}
-
-export const localUser = user.load() || createUser()
+export const localUser = loadUser() || createUser(randomUserName())
