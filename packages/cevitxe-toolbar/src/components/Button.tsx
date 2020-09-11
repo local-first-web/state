5c723c449eaf0674560e3dd50450f570adae0ebb
Herb Caudill <herb@devresults.com>
2020-09-11 15:35:10 +0200

    cevitxe-toolbar/components/Button.tsx:

--- packages/cevitxe-toolbar/src/components/Button.tsx
+++ packages/cevitxe-toolbar/src/components/Button.tsx
@@ -9,8 +9,10 @@ export const Button: FunctionComponent<any> = ({ children, className, ...props }
       type="button"
       className={`block py-2 px-4  
         font-mono text-sm leading-none uppercase text-gray-900 
-        bg-white hover:bg-blue-100 
-        rounded-md border border-gray-400 focus:outline-none focus:border-blue-500
+        bg-white 
+        hover:bg-blue-100 hover:bg-bl
+        rounded-md border border-gray-400 
+        transition ease-in-out duration-150
         cursor-pointer 
         ${className}`}
       {...props}
