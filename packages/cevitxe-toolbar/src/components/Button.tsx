cda92f9d9ec8b3e2a57715a41dc140b6aad0c7e2
HerbCaudill <herb@devresults.com>
2020-09-15 14:11:46 +0200

    cevitxe-toolbar/components/Button.tsx: variants

--- packages/cevitxe-toolbar/src/components/Button.tsx
+++ packages/cevitxe-toolbar/src/components/Button.tsx
@@ -1,26 +1,56 @@
-﻿/** @jsx jsx */
-import { jsx } from '@emotion/core'
-import { FunctionComponent } from 'react'
+﻿import React from 'react'
 
-export const Button: FunctionComponent<any> = ({ children, className, ...props }) => {
+export const Button: React.FC<ButtonProps & ExtendElement<'button'>> = ({
+  variant = 'default',
+  size = 'default',
+  className = '',
+  children,
+  ...props
+}) => {
   return (
-    <button
-      role="button"
-      type="button"
-      className={`
-        inline-flex items-center 
-        px-3 py-2 h-8
-        border border-gray-300 
-        text-sm leading-tight font-bold rounded-md text-gray-700 
-        bg-white 
-        hover:text-gray-900 hover:bg-blue-100
-        focus:outline-none focus:border-blue-300 focus:shadow-outline-blue 
-        active:text-gray-800 active:bg-gray-50 
-        transition ease-in-out duration-150
-        ${className}`}
-      {...props}
-    >
-      {children}
-    </button>
+    <span className="inline-flex rounded-md shadow-sm">
+      <button
+        role="button"
+        type="button"
+        className={`
+          inline-flex items-center 
+          
+          font-bold 
+          focus:outline-none  
+          active:text-neutral-800 active:bg-neutral-50 
+          transition ease-in-out duration-150
+          ${sizes[size]}
+          ${variants[variant]}
+          ${className}`}
+        {...props}
+      >
+        {children}
+      </button>
+    </span>
   )
 }
+
+const sizes = {
+  xs: 'px-2.5 py-1.5 text-xs leading-4 rounded',
+  sm: 'px-3 py-2 text-sm leading-4 rounded-md',
+  default: 'px-4 py-2 text-sm leading-5 rounded-md',
+  lg: 'px-4 py-2 text-base leading-6 rounded-md',
+  xl: 'px-6 py-3 text-base leading-6 rounded-md',
+}
+type ButtonSize = keyof typeof sizes
+
+const variants = {
+  default: `border border-neutral-300 text-neutral-700 bg-white hover:text-neutral-500 focus:border-primary-300 focus:shadow-outline-primary active:text-neutral-800 active:bg-neutral-50 `,
+  primary: `border border-transparent text-white bg-primary-600 hover:bg-primary-500 focus:border-primary-700 focus:shadow-outline-primary active:bg-primary-700 `,
+  secondary: `border border-transparent text-secondary-700 bg-secondary-100 hover:bg-secondary-50 focus:border-secondary-300 focus:shadow-outline-secondary active:bg-secondary-200 `,
+}
+type ButtonVariant = keyof typeof variants
+
+type ButtonProps = {
+  variant?: ButtonVariant
+  size?: ButtonSize
+  className?: string
+}
+
+type JsxElementName = keyof JSX.IntrinsicElements
+type ExtendElement<t extends JsxElementName> = React.PropsWithoutRef<JSX.IntrinsicElements[t]>
