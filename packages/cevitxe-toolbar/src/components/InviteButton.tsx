534a63550be1ea29c3c455a6ddf3df08f87a6228
HerbCaudill <herb@devresults.com>
2020-10-12 12:05:20 +0200

    cevitxe-toolbar/components/InviteButton.tsx:

--- packages/cevitxe-toolbar/src/components/InviteButton.tsx
+++ packages/cevitxe-toolbar/src/components/InviteButton.tsx
@@ -9,19 +9,22 @@ import {
   ModalFooter,
   ModalHeader,
 } from '@windmill/react-ui'
-import React, { FunctionComponent, useState } from 'react'
+import React, { FC, useState } from 'react'
 
-const ModalIcon: FunctionComponent = ({ children }) => (
-  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 text-xl rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
+const ModalIcon: FC = ({ children }) => (
+  <div
+    className="
+      mx-auto
+      flex items-center justify-center 
+      h-12 w-12
+      text-xl 
+      rounded-full bg-blue-100 "
+  >
     {children}
   </div>
 )
 
-const ModalContent: FunctionComponent = ({ children }) => (
-  <div className="-mt-1 text-center sm:mt-0 sm:ml-4 sm:text-left">{children}</div>
-)
-
-export const InviteButton: FunctionComponent = () => {
+export const InviteButton: FC = () => {
   const [isOpen, setIsOpen] = useState(false)
 
   const open = () => {
@@ -34,24 +37,23 @@ export const InviteButton: FunctionComponent = () => {
 
   return (
     <div className="relative">
-      <Button layout="outline" onFocus={open}>
+      <Button layout="outline" onClick={open}>
         💌 Invite
       </Button>
-      <Modal isOpen={isOpen} onClose={close} className="w-24 bg-yellow">
-        <ModalBody>
-          <div className="flex flex-row">
-            <ModalIcon>💌</ModalIcon>
-            <div className="ml-3 -mt-1">
-              <ModalHeader>Who do you want to invite?</ModalHeader>
-              <Label>
-                <p>Enter their username</p>
-                <Input id="username" autoFocus={isOpen} className="mt-1" />
-                <HelperText>e.g. mister.kittycat</HelperText>
-              </Label>
-            </div>
+      <Modal isOpen={isOpen} onClose={close} style={{ width: 400 }}>
+        <ModalBody className="flex flex-row">
+          <ModalIcon>💌</ModalIcon>
+          <div className="ml-2 flex-grow">
+            <ModalHeader>Who do you want to invite?</ModalHeader>
+            <Label>
+              <p>Enter their username</p>
+              <Input id="username" autoFocus={isOpen} className="mt-1" />
+              <HelperText>
+                Example: <code>mister.kittycat</code>
+              </HelperText>
+            </Label>
           </div>
         </ModalBody>
-
         <ModalFooter>
           <Button layout="outline" className="w-full sm:w-auto" onClick={close}>
             Cancel
