f6ba272a9a10d0ca0627857b23ced4f35b47939a
HerbCaudill <herb@devresults.com>
2020-12-16 12:50:48 +0100

    remove unused css

--- packages/cevitxe-example-grid/src/index.tsx
+++ packages/cevitxe-example-grid/src/index.tsx
@@ -1,7 +1,6 @@
 import 'core-js'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import 'cevitxe-toolbar/dist/styles.css'
 
 import { Shell } from './components/Shell'
