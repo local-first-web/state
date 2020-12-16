1a0d827456f40c656cd79685b3dbae876063e051
HerbCaudill <herb@devresults.com>
2020-12-16 12:50:41 +0100

    cevitxe/Connection.ts:

--- packages/cevitxe/src/Connection.ts
+++ packages/cevitxe/src/Connection.ts
@@ -8,13 +8,35 @@ import { Message } from './Message'
 
 const log = debug('cevitxe:connection')
 
+/*
+
+Since one Connection has exactly one Synchronizer and nothing else talks to a Synchronizer, 
+maybe it makes sense to merge these two objects? 
+
+A Connection needs 
+- a WebSocket 
+  - the WebSocket has a peer at the other end
+  - when a message is received from the WebSocket, the Synchronizer handles it
+- a Repo 
+  - the Repo tells the Connection about local changes
+  - the Connection tells the Repo about remote changes
+- a Dispatch function (from a Redux store)
+  - the Connection calls the Dispatch function to notify the store about remote changes
+
+Internally the connection creates a Synchronizer
+- the Synchronizer guarantees that our Repo and the peer's Repo will eventually have the same contents
+
+
+
+*/
+
 /**
  * A `Connection` keeps one local document synchronized with one peer's replica of the same
  * document. It uses `Synchronizer` for the synchronization logic, and integrates it with Cevitxe's
  * networking stack and with the Redux store.
  */
 export class Connection extends EventEmitter {
-  private Synchronizer: Synchronizer
+  private synchronizer: Synchronizer
   private peerSocket: WebSocket | null
   private dispatch: Dispatch<AnyAction>
   private repo: Repo<any>
@@ -28,15 +50,17 @@ export class Connection extends EventEmitter {
 
     this.peerSocket.onmessage = this.receive.bind(this)
 
-    this.Synchronizer = new Synchronizer(this.repo, this.send)
-    this.Synchronizer.open().then(() => this.emit('ready'))
+    this.synchronizer = new Synchronizer(this.repo, this.send)
+    this.synchronizer.open().then(() => this.emit('ready'))
   }
 
   receive = async ({ data }: any) => {
     const message = JSON.parse(data.toString())
     log('receive %o', message)
     this.emit('receive', message)
-    await this.Synchronizer.receive(message) // Synchronizer will update repo directly
+    await this.synchronizer.receive(message) // Synchronizer will update repo directly
+
+    // TODO: This seems janky
     // hit the dispatcher to force it to pick up
     this.dispatch({ type: RECEIVE_MESSAGE_FROM_PEER })
   }
