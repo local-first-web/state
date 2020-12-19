d7047bba7df00aec3a0eeb8986803c6a659e92cd
HerbCaudill <herb@devresults.com>
2020-12-19 11:17:56 +0100

    cevitxe-storage-abstract/index.ts: 'async' modifier cannot be used with 'abstract' modifier

--- packages/cevitxe-storage-abstract/src/index.ts
+++ packages/cevitxe-storage-abstract/src/index.ts
@@ -8,15 +8,15 @@ export abstract class StorageAdapter {
     this.storageKey = `cevitxe_${databaseName}_${discoveryKey.substr(0, 12)}`
   }
 
-  abstract async open(): Promise<void>
-  abstract async close(): Promise<void>
-  abstract async hasData(): Promise<boolean>
+  abstract open(): Promise<void>
+  abstract close(): Promise<void>
+  abstract hasData(): Promise<boolean>
 
   abstract changes(): AsyncIterableIterator<ChangeSet>
-  abstract async getChanges(documentId: string): Promise<ChangeSet[]>
-  abstract async appendChanges(changeSet: ChangeSet): Promise<void>
+  abstract getChanges(documentId: string): Promise<ChangeSet[]>
+  abstract appendChanges(changeSet: ChangeSet): Promise<void>
 
   abstract snapshots(): AsyncIterableIterator<SnapshotRecord>
-  abstract async putSnapshot(snapshotRecord: SnapshotRecord): Promise<void>
-  abstract async deleteSnapshot(snapshotId: string): Promise<void>
+  abstract putSnapshot(snapshotRecord: SnapshotRecord): Promise<void>
+  abstract deleteSnapshot(snapshotId: string): Promise<void>
 }
