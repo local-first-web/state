0f6f5415f8119840dbc794995a41e200a15cb03a
HerbCaudill <herb@devresults.com>
2020-12-19 14:48:11 +0100

    state/constants.ts: signal -> relay

--- packages/state/src/constants.ts
+++ packages/state/src/constants.ts
@@ -2,7 +2,7 @@
 export const RECEIVE_MESSAGE_FROM_PEER = '@localfirst/state/RECEIVE_MESSAGE_FROM_PEER'
 export const INITIALIZE = '@localfirst/state/INITIALIZE'
 
-export const DEFAULT_SIGNAL_SERVERS = [`ws://${window.location.hostname}:8080`] // default public signaling server
+export const DEFAULT_RELAYS = [`ws://${window.location.hostname}:8080`] // default public relay
 
 // deleted flag
 export const DELETED = '__DELETED'
